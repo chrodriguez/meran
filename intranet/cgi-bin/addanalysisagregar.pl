e71e1ed42145e8ab5123e92c536d4b329876e1ac
Matias Pagano <matiasp@mail.linti.unlp.edu.ar>
2008-03-12 15:45:50 +0000

    analiticas
    git-svn-id: svn+ssh://163.10.10.34/var/repositorioV2-V3@201 107f0ce8-b514-0410-b978-fdcfc4524ee3

--- intranet/cgi-bin/addanalysisagregar.pl
+++ intranet/cgi-bin/addanalysisagregar.pl
@@ -58,7 +58,7 @@ my ($template, $loggedinuser, $cookie)
 			     query => $input,
 			     type => "intranet",
 			     authnotrequired => 0,
-			     flagsrequired => {acquisition => 1},
+			     flagsrequired => {editcatalogue => 1},
 			     debug => 1,
 			     });
 ( $biblioitemcount, @biblioitems ) = &getbiblioitembybiblionumber($bibnum);
