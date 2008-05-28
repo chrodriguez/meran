15bc830c25cf7b1259d8be53f70d66c31bf824db
Miguel Carbone <mcarbone@mail.linti.unlp.edu.ar>
2008-05-28 19:56:17 +0000

    git-svn-id: svn+ssh://163.10.10.34/var/repositorioV2-V3@437 107f0ce8-b514-0410-b978-fdcfc4524ee3

---
+++ opac/cgi-bin/opac-MARCDetail.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+use strict;
+require Exporter;
+use CGI;
+# use C4::Search;
+use C4::Auth;
+use C4::Interface::CGI::Output;
+use HTML::Template;
+
+use C4::AR::Catalogacion;
+use C4::AR::Busquedas;
+
+my $input=new CGI;
+
+my ($template, $borrowernumber, $cookie) 
+    = get_template_and_user({template_name => "opac-MARCdetail.tmpl",
+			     query => $input,
+			     type => "opac",
+			     authnotrequired => 1,
+			     flagsrequired => {borrow => 1},
+			     });
+
+my $idNivel3=$input->param('id3');
+
+# my $nivel1=&buscarNivel1($idNivel1); #C4::AR::Catalogacion;
+# my @autor=C4::Search::getautor($nivel1->{'autor'});
+# my @nivel1Loop= &detalleNivel1_copia($idNivel1, $nivel1, 'opac');
+# my @nivel2Loop= &detalleNivel2_copia($idNivel1, 'opac');
+# 
+#  
+# for (my $i=0; $i < scalar(@nivel2Loop); $i++){
+#  	@nivel2Loop[$i]->{'loopnivel1'}= \@nivel1Loop;
+# }
+
+my @nivel2Loop= &MARCDetail($idNivel3);
+
+$template->param(
+ 	loopnivel2 => \@nivel2Loop,
+);
+
+output_html_with_http_headers $input, $cookie, $template->output;
