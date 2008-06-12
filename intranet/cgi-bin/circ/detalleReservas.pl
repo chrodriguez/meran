4972a5b2d0eed7a2bea248bded765fe16df13e45
Miguel Carbone <mcarbone@mail.linti.unlp.edu.ar>
2008-06-12 15:20:52 +0000

    git-svn-id: svn+ssh://163.10.10.34/var/repositorioV2-V3@492 107f0ce8-b514-0410-b978-fdcfc4524ee3

---
+++ intranet/cgi-bin/circ/detalleReservas.pl
@@ -0,0 +1,104 @@
+#!/usr/bin/perl
+# Please use 8-character tabs for this file (indents are every 4 characters)
+
+#written 8/5/2002 by Finlay
+#script to execute issuing of books
+
+# Copyright 2000-2002 Katipo Communications
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Koha; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+# Suite 330, Boston, MA  02111-1307 USA
+
+use strict;
+use CGI;
+use C4::Circulation::Circ2;
+use C4::Output;
+# use DBI;
+use C4::Auth;
+use C4::Interface::CGI::Output;
+use C4::Koha;
+use HTML::Template;
+use C4::Date;
+use C4::AR::Reservas;
+use Date::Manip;
+
+my $input=new CGI;
+
+
+
+
+my ($template, $loggedinuser, $cookie) = get_template_and_user ({
+	template_name	=> 'circ/detalleReservas.tmpl',
+	query		=> $input,
+	type		=> "intranet",
+	authnotrequired	=> 0,
+	flagsrequired	=> { circulate => 1 },
+    });
+
+
+my $borrowernumber= $input->param('borrowernumber');
+
+
+
+
+# now the reserved items....
+my ($rescount, $reserves) = C4::AR::Reservas::DatosReservas ($borrowernumber);
+my @realreserves;
+my @waiting;
+my $rcount = 0;
+my $wcount = 0;
+my $clase1='par';
+my $clase2='par';
+my $dateformat = C4::Date::get_date_format();
+
+foreach my $res (@$reserves) {
+
+	$res->{'rreminderdate'} = format_date($res->{'rreminderdate'},$dateformat);
+	$res->{'rnotificationdate'}  = format_date($res->{'rnotificationdate'},$dateformat);
+	$res->{'rreminderdate'}  = format_date($res->{'rreminderdate'},$dateformat);
+
+	#Corregido 13/03/07 Miguel
+	#obtengo el autor
+#  	my $author=getautor($res->{'rautor'});
+	#guardo el Apellido, Nombre del autor
+# 	$res->{'rautor'} = $author->{'nomCompleto'}; #le paso Apellido y Nombre
+	#guardo el ID de autor para luego hacer busqueda por este campo
+# 	$res->{'id'} = $author->{'id'}; #le paso el Id del autor  
+
+	if ($res->{'estado'} eq 'E') {
+# 		$res->{'rbranch'} = $branches->{$res->{'rbranch'}}->{'branchcode'};
+		push @realreserves, $res;
+		$rcount++;
+	} else { 
+		push @waiting, $res;
+		$wcount++;
+	} 
+}#end for
+
+$template->param(
+	
+		RESERVES => \@realreserves,
+		reserves_count => $rcount,
+		WAITRESERVES => \@waiting,
+		waiting_count => $wcount,
+
+);
+
+output_html_with_http_headers $input, $cookie, $template->output;
+
+
+# Local Variables:
+# tab-width: 8
+# End:
