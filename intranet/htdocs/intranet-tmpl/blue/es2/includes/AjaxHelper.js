c24c604c8781cc3bb0aaacc23304850d0fbe6c4a
Damian Di Costanso <dico>
2008-06-19 14:04:20 +0000

    git-svn-id: svn+ssh://163.10.10.34/var/repositorioV2-V3@512 107f0ce8-b514-0410-b978-fdcfc4524ee3

---
+++ intranet/htdocs/intranet-tmpl/blue/es2/includes/AjaxHelper.js
@@ -0,0 +1,75 @@
+/*
+ * LIBRERIA AjaxhHelper v 1.0.1
+ * Esta es una libreria creada para el sistema KOHA
+ * Para poder utilizarla es necesario incluir en el tmpl la libreria jquery.js
+ * @author Carbone Miguel, Di Costanzo Damian
+ * Fecha de creacion 22/05/2008
+ *
+ */
+
+
+
+
+
+function AjaxHelper(fncUpdateInfo, fncInit){
+
+	this.ini= '';		//para manejar el actual del paginador
+	this.funcion= '';	//nombre de funcion que tiene q conocer el paginador
+	this.url= '/cgi-bin/koha/busquedas/busqueda.pl';
+	this.orden= '';		//para ordenar los resultados
+	this.debug= false;	
+	this.onComplete= fncUpdateInfo;  //se ejecuta cuando se completa el ajax
+	this.onBeforeSend= fncInit;	//se ejecuta antes de consultar al servidor con ajax
+
+	this.sendToServer= function(){
+
+			//se ejecuta el ajax
+			this.ajaxCallback(this);
+
+	}//end sendToServer
+
+	this.sort= function(ord){
+
+			if(this.debug){
+ 				console.log("SearchHelper => sort: " + ord);
+ 			}
+			
+			//seteo el orden de los resultados
+			this.orden= ord;
+			//se envia la consulta
+			this.sendToServer();
+	}
+
+	this.changePage= function(ini){
+
+				if(this.debug){
+ 					console.log("SearchHelper => changePage: " + ini);
+ 				}
+
+				this.ini= ini;
+				this.sendToServer();
+
+	}
+
+	this.ajaxCallback= function(helper){
+			
+			var params= "obj="+JSONstring.make(helper);
+
+			if(this.debug){
+ 				console.log("SearchHelper => ajaxCallback \n" + params);
+			}
+
+	
+			$.ajax({	type: "POST", 
+					url: helper.url,
+					data: params,
+ 					beforeSend: helper.onBeforeSend,
+					complete: function(ajax){
+ 						helper.onComplete(ajax.responseText);
+  					}
+				});
+	}
+
+}
+
+
