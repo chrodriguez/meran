1c15129b307eab31999c034ccf7a4e9b5e47a795
Miguel Carbone <mcarbone@mail.linti.unlp.edu.ar>
2008-07-28 17:21:45 +0000

    git-svn-id: svn+ssh://163.10.10.34/var/repositorioV2-V3@631 107f0ce8-b514-0410-b978-fdcfc4524ee3

---
+++ intranet/cgi-bin/estanteVirtual.pl
@@ -0,0 +1,85 @@
+#!/usr/bin/perl
+
+
+use strict;
+use CGI;
+use C4::BookShelves;
+use C4::Circulation::Circ2;
+use C4::Auth;
+use C4::Interface::CGI::Output;
+use C4::AR::Utilidades;
+
+my $input = new CGI;
+my $type='public';
+
+
+my ($template, $loggedinuser, $cookie)
+	= get_template_and_user({template_name => "estanteVirtual.tmpl",
+					query => $input,
+					type => "intranet",
+					authnotrequired => 0,
+					flagsrequired => {catalogue => 1},
+					});
+
+
+my $themelang = $template->param('themelang');
+open(A, ">>/tmp/debug.txt");
+print A "opac-estantevirtual themeland $themelang \n";
+close(A);
+
+#Para mandar la dir de mail
+my ($borr, $flags) = getpatroninformation(undef, $loggedinuser);
+if ($borr and ($borr->{'emailaddress'})){  $template->param(MAIL =>$borr->{'emailaddress'} ); }
+
+my %shelflist;
+
+my $obj=$input->param('obj');
+$obj= &C4::AR::Utilidades::from_json_ISO($obj);
+
+my $funcion= $obj->{'funcion'};
+my $ini= $obj->{'ini'}||'';
+my $count=0;
+
+my ($ini,$pageNumber,$cantR)= &C4::AR::Utilidades::InitPaginador($ini);
+
+  %shelflist = &GetShelfList($type);
+  ($count)= &getshelfListCount($type);
+
+&C4::AR::Utilidades::crearPaginador($template, $count, $cantR, $pageNumber,$funcion);
+
+
+$template->param({LibraryName => C4::Context->preference("LibraryName")});
+
+
+my $color='';
+my @shelvesloop;
+
+my @key=sort { noaccents($shelflist{$a}->{'shelfname'}) cmp noaccents($shelflist{$b}->{'shelfname'}) } keys(%shelflist);
+
+
+$ini= $ini*$cantR;
+my $fin= $cantR-1+$ini;
+my $cantEstantes= scalar(@key);
+if($fin > $cantEstantes){
+#si hay menos estantes para mostrar que renglones por pagina, el limite es la cantdad de estantes para mostrar
+	$fin= $cantEstantes - 1;
+}
+my @keyAux=@key[$ini..$fin];
+
+foreach my $element (@keyAux) {
+		my %line;
+		$line{'shelf'}=$element;
+		#los datos del padre, sirve para la busqueda
+#                 $line{'numberparent'}=$shelflist{$element}->{'numberparent'};
+		#los datos del padre,sirve para la busqueda
+#                 $line{'nameparent'}=$shelflist{$element}->{'nameparent'};
+		$line{'shelfname'}=$shelflist{$element}->{'shelfname'};
+		$line{'shelfbookcount'}=$shelflist{$element}->{'count'};
+		$line{'countshelf'}=$shelflist{$element}->{'countshelf'} ;
+
+		push (@shelvesloop, \%line);
+}
+
+$template->param(shelvesloop => \@shelvesloop);
+
+output_html_with_http_headers $input, $cookie, $template->output;
---
+++ intranet/cgi-bin/estanteVirtualDB.pl
@@ -0,0 +1,81 @@
+#!/usr/bin/perl
+
+use strict;
+use CGI;
+use C4::BookShelves;
+use C4::Auth;
+use C4::Interface::CGI::Output;
+
+my $input=new CGI;
+
+my ($loggedinuser, $cookie, $sessionID) = checkauth($input, 0,{borrow => 1});
+my $borrowernumber=getborrowernumber($loggedinuser);
+
+my $obj=$input->param('obj');
+$obj=C4::AR::Utilidades::from_json_ISO($obj);
+my $tipo= $obj->{'tipo'};
+
+
+if($tipo eq "VER_ESTANTE"){
+
+	my ($template, $loggedinuser, $cookie)
+	= get_template_and_user({template_name => "verEstanteVirtual.tmpl",
+					query => $input,
+					type => "intranet",
+					authnotrequired => 0,
+					flagsrequired => {catalogue => 1},
+					});
+
+	
+	my $shelves= $obj->{'shelves'};
+	my $funcion= $obj->{'funcion'};
+	my $orden=$obj->{'orden'}||'title';
+	my $ini= $obj->{'ini'}||'';
+	
+	my ($ini,$pageNumber,$cantR)= &C4::AR::Utilidades::InitPaginador($ini);
+	
+	my ($cantidad, @shelvesloop)= C4::BookShelves::viewshelf($shelves,$ini,$cantR);
+	
+	&C4::AR::Utilidades::crearPaginador($template, $cantidad, $cantR, $pageNumber,$funcion);
+	
+	$template->param(
+				shelvesloopshelves => @shelvesloop,
+				pagetitle => "Estantes Virtuales",
+				shelves => 1,
+	);
+	
+	# print  $template->output;
+	output_html_with_http_headers $input, $cookie, $template->output;
+}
+
+
+if($tipo eq "VER_SUBESTANTE"){
+
+	my ($template, $loggedinuser, $cookie)
+	= get_template_and_user({template_name => "verSubEstanteVirtual.tmpl",
+					query => $input,
+					type => "intranet",
+					authnotrequired => 0,
+					flagsrequired => {catalogue => 1},
+					});
+	
+	my $shelves= $obj->{'shelves'};
+	my $funcion= $obj->{'funcion'};
+	my $orden=$obj->{'orden'}||'title';
+	my $ini= $obj->{'ini'}||'';
+	
+	my ($ini,$pageNumber,$cantR)= &C4::AR::Utilidades::InitPaginador($ini);
+	
+	my ($cantidad, @shelvesloop)= C4::BookShelves::viewshelfContent($shelves,$ini,$cantR,$orden);
+	
+	&C4::AR::Utilidades::crearPaginador($template, $cantidad, $cantR, $pageNumber,$funcion);
+	
+	$template->param(
+				bitemsloop => @shelvesloop,
+				pagetitle => "Estantes Virtuales",
+				shelves => 1,
+	);
+	
+	# print  $template->output;
+	output_html_with_http_headers $input, $cookie, $template->output;
+}
