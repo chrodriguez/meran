1299369a25ddf57f668af9788db9012e89054dc4
Miguel Carbone <mcarbone@mail.linti.unlp.edu.ar>
2008-09-26 17:21:29 +0000

    git-svn-id: svn+ssh://163.10.10.34/var/repositorioV2-V3@668 107f0ce8-b514-0410-b978-fdcfc4524ee3

--- intranet/cgi-bin/about.pl
+++ intranet/cgi-bin/about.pl
@@ -9,7 +9,7 @@ use C4::Auth;
 use C4::Context;
 use CGI;
 
-#agrego una modificacion para testear el plugin del mantisssssssssssssdddfasdfasdfs
+#agrego una modificacion para testear el plugin del mantisssssssssssssdddfas
 
 my $dbh = C4::Context->dbh;
 my $query = new CGI;
