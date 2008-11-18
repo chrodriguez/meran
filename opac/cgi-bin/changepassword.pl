8971a3c7087f17f072eb901f048d37b20bbcc9c0
Miguel Carbone <mcarbone@mail.linti.unlp.edu.ar>
2008-11-18 13:41:16 +0000

    git-svn-id: svn+ssh://163.10.10.34/var/repositorioV2-V3@787 107f0ce8-b514-0410-b978-fdcfc4524ee3

---
+++ opac/cgi-bin/changepassword.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+use strict;
+require Exporter;
+
+use C4::Output;  # contains gettemplate
+use C4::Auth;
+use C4::Context;
+use CGI;
+
+my $query = new CGI;
+
+my ($template, $params)= C4::Output::gettemplate("changepassword.tmpl", 'opac');
+
+
+C4::Auth::output_html_with_http_headers($query, $template, $params);
+
+
