ed3e9bdcf6740403a8b20dca775e744f89ad2565
Matias Pagano <matiasp@mail.linti.unlp.edu.ar>
2008-12-03 13:25:42 +0000

    script i18n
    git-svn-id: svn+ssh://163.10.10.34/var/repositorioV2-V3@826 107f0ce8-b514-0410-b978-fdcfc4524ee3

--- intranet/scripts/search-i18n.pl
+++ intranet/scripts/search-i18n.pl
@@ -9,34 +9,52 @@ my $output_po = $ARGV[1]; #Archivo PO resultado
 
 find (\&process, $directory);
 
+sub trim
+{
+ my ($string)=@_;
+$string =~ s/^\s+//;
+$string =~ s/\s+$//;
+return $string;
+}
+
 sub process
 {
     my @outLines;  #Arreglo de POs
     my $line;      #Linea leida.
-
+    my @matches;
     #Buscamos solo los  .tmpl
     if ( $File::Find::name =~ /\.tmpl$/ ) {
 	print "Procesando $File::Find::name\n";
         open (FILE, $File::Find::name ) or die "No se pudo abrir el archivo: $!";
 
         while ( $line = <FILE> ) {
-
-	 if ($line =~ /(\[%)\s*["]*\s*([^"]*)\s*["]*\s*(\|\s*i18n\s*%])/)
+	@matches = ($line =~ /\[%\s*['"]*\s*([^{",\|,'}]*)\s*['"]*\s*\|\s*i18n\s*%]/g);
+	foreach my $m (@matches)
 	{
-	   my $po = new Locale::PO();
-	   $po->msgid("$2");
-           $po->msgstr("");
-           $po->comment("$File::Find::name");
-	
-	   push(@outLines, $po);
+	 my $str=&trim($m);
+	 my $exists=0;
+	foreach my $p (@outLines) {
+				if ($p->msgid() eq '"'.$str.'"'){$exists=1;} #Busco si no existe y con los ""
+				} 
+
+	 if($exists==0){
+	 	my $po = new Locale::PO();
+	 	$po->msgid($str);
+         	$po->msgstr("");
+         	$po->comment("$File::Find::name");
+		 push(@outLines, $po);
+	 }
+
 	}
 
         }
         close FILE;
 
+
+
     #Salvamos el po
     open( OUT, ">>$output_po" ) or return undef;
-    foreach (@outLines)  { print OUT $_->dump(); }
+    foreach (@outLines)  { print OUT $_->dump();}
     close OUT;
 
     undef( @outLines );
