813ae1390cc45cf17affca21cebe74a2aec44584
Matias Pagano <matiasp@mail.linti.unlp.edu.ar>
2008-12-30 18:08:24 +0000

    mono
    git-svn-id: svn+ssh://163.10.10.34/var/repositorioV2-V3@844 107f0ce8-b514-0410-b978-fdcfc4524ee3

---
+++ intranet/scripts/mapear_modelo.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+use Rose::DB::Object::Loader;
+
+my $loader = Rose::DB::Object::Loader->new(
+                   db_dsn       => 'dbi:mysql:dbname=Econo-V2V3;host=localhost',
+                   db_username  => 'kohaadmin',
+                   db_password  => 'patyalmicro',
+                   db_options   => { AutoCommit => 1},
+		  class_prefix => 'C4::Modelo'
+                    );
+$loader->make_modules(  module_dir => "/usr/local/koha/intranet/modules/",
+                                        );
