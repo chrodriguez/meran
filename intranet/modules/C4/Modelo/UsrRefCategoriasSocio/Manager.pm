ce7044c291897c500b412ae636d1f3eddad49760
Miguel Carbone <mcarbone@mail.linti.unlp.edu.ar>
2009-01-07 13:33:28 +0000

    git-svn-id: svn+ssh://163.10.10.34/var/repositorioV2-V3@892 107f0ce8-b514-0410-b978-fdcfc4524ee3

---
+++ intranet/modules/C4/Modelo/UsrRefCategoriasSocio/Manager.pm
@@ -0,0 +1,13 @@
+package C4::Modelo::UsrRefCategoriasSocio::Manager;
+
+use strict;
+
+use base qw(Rose::DB::Object::Manager);
+
+use C4::Modelo::UsrRefCategoriasSocio;
+
+sub object_class { 'C4::Modelo::UsrRefCategoriasSocio' }
+
+__PACKAGE__->make_manager_methods('usr_ref_categoria_socio');
+
+1;
