bffb05dbf4f4170690d4f023f2f97fb60f885c87
Miguel Carbone <mcarbone@mail.linti.unlp.edu.ar>
2009-03-06 19:19:13 +0000

    git-svn-id: svn+ssh://163.10.10.34/var/repositorioV2-V3@1135 107f0ce8-b514-0410-b978-fdcfc4524ee3

---
+++ intranet/cgi-bin/utils/utilsDB.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl
+
+
+use strict;
+use CGI;
+use C4::Auth;
+use C4::Interface::CGI::Output;
+use C4::AR::Utilidades;
+use C4::AR::Catalogacion;
+use JSON;
+
+my $input = new CGI;
+
+
+my $obj=$input->param('obj');
+$obj=C4::AR::Utilidades::from_json_ISO($obj);
+
+my $tipoAccion= $obj->{'tipoAccion'}||"";
+
+
+if($tipoAccion eq "GENERAR_ARREGLO_TABLA_REF"){
+
+    my ($tablaRef_array) = C4::AR::Referencias::obtenerTablasDeReferenciaAsString();
+    
+    my ($infoOperacionJSON) = to_json($tablaRef_array);
+
+    
+    print $input->header;
+    print $infoOperacionJSON;
+
+}
+
+elsif($tipoAccion eq "GENERAR_ARREGLO_UI"){
+
+    my ($ui_array_ref) = C4::AR::Referencias::obtenerUnidadesDeInformacion();
+    my %select_ui;
+    my @infoCombo;
+
+    foreach my $ui (@$ui_array_ref) {
+#         $select_ui{$ui->id_ui}= $ui->nombre;
+        my %select_ui;
+        $select_ui{'clave'}= $ui->id_ui;
+        $select_ui{'valor'}= $ui->nombre;
+        push (@infoCombo, \%select_ui);
+    }
+
+#     my ($infoOperacionJSON) = to_json(\%select_ui);
+    my ($infoOperacionJSON) = to_json(\@infoCombo);
+    
+    print $input->header;
+    print $infoOperacionJSON;
+}
+
+
