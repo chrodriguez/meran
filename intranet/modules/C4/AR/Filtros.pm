252c44d2be5d8deed4f04fa8af4313fdb3e26092
Miguel Carbone <mcarbone@mail.linti.unlp.edu.ar>
2009-03-13 21:33:47 +0000

    git-svn-id: svn+ssh://163.10.10.34/var/repositorioV2-V3@1172 107f0ce8-b514-0410-b978-fdcfc4524ee3

--- intranet/modules/C4/AR/Filtros.pm
+++ intranet/modules/C4/AR/Filtros.pm
@@ -52,36 +52,36 @@ Se le parametriza si el combo es para la INTRA u OPAC
 =cut
 sub setComboLang {
 
- 	my ($type) = @_;
-	my $session = CGI::Session->load();
+    my ($type) = @_;
+    my $session = CGI::Session->load();
 # open(A, ">>/tmp/debug.txt");
 # print A "desde putHTML: \n";
-	my $html= '';
-	my $lang_Selected= $session->param('locale');
+    my $html= '';
+    my $lang_Selected= $session->param('locale');
 ## FIXME falta recuperar esta info desde la base es_ES => Español, ademas estaria bueno agregarle la banderita
-	my @array_lang= ('es_ES', 'en_EN', 'nz_NZ', 'jp_JP');
-	my $i;
+    my @array_lang= ('es_ES', 'en_EN', 'nz_NZ', 'jp_JP');
+    my $i;
 
-# 	if($type eq 'OPAC'){
-# 		$html="<form id='formLang' action='/cgi-bin/koha/opac-language.pl' method='POST'>";
-# 	}else{
-# 		$html="<form id='formLang' class='selectLang' action='/cgi-bin/koha/intra-language.pl' method='POST'>";
-# 	}
-# 
-# 	$html .="<input id='lang_server' name='lang_server' type='hidden' value=''>";	
-# 	$html .="<input id='url' name='url' type='hidden' value=''>";
-	$html .="<select id='language' class='selectLang' onChange='cambiarIdioma()'>";
+    if($type eq 'OPAC'){
+        $html="<form id='formLang' action='/cgi-bin/koha/opac-language.pl' method='POST'>";
+    }else{
+        $html="<form id='formLang' action='/cgi-bin/koha/intra-language.pl' method='POST'>";
+    }
 
-	for($i=0;$i<scalar(@array_lang);$i++){
-		if($session->param('locale') eq @array_lang[$i]){
-			$html .="<option value='".@array_lang[$i]."' selected='selected'>".@array_lang[$i]."</option>"; 
-		}else{
-			$html .="<option value='".@array_lang[$i]."'>".@array_lang[$i]."</option>";
-		}
-	}
+    $html .="<input id='lang_server' name='lang_server' type='hidden' value=''>";   
+    $html .="<input id='url' name='url' type='hidden' value=''>";
+    $html .="<select id='language' onChange='cambiarIdioma()'>";
 
-	$html .="</select>";
-# 	$html .="</form>";
+    for($i=0;$i<scalar(@array_lang);$i++){
+        if($session->param('locale') eq @array_lang[$i]){
+            $html .="<option value='".@array_lang[$i]."' selected='selected'>".@array_lang[$i]."</option>"; 
+        }else{
+            $html .="<option value='".@array_lang[$i]."'>".@array_lang[$i]."</option>";
+        }
+    }
+
+    $html .="</select>";
+    $html .="</form>";
 # close(A);
- 	return $html;
+    return $html;
 }
