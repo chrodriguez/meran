60efb85357734a1abb47d670c86c8b8460ef8bee
Miguel Carbone <mcarbone@mail.linti.unlp.edu.ar>
2009-04-20 12:59:18 +0000

    git-svn-id: svn+ssh://163.10.10.34/var/repositorioV2-V3@1326 107f0ce8-b514-0410-b978-fdcfc4524ee3

--- intranet/cgi-bin/busquedas/MARCDetalle.pl
+++ intranet/cgi-bin/busquedas/MARCDetalle.pl
@@ -10,12 +10,12 @@ use C4::AR::Catalogacion;
 
 my $input=new CGI;
 
-my ($template, $loggedinuser, $cookie) = get_template_and_user({
-	template_name   => ('busquedas/MARCDetalle.tmpl'),
-	query           => $input,
-	type            => "intranet",
-	authnotrequired => 0,
-	flagsrequired   => {catalogue => 1},
+my ($template, $session, $t_params) = get_template_and_user({
+																template_name   => ('busquedas/MARCDetalle.tmpl'),
+																query           => $input,
+																type            => "intranet",
+																authnotrequired => 0,
+																flagsrequired   => {catalogue => 1},
     });
 
 my $obj=C4::AR::Utilidades::from_json_ISO($input->param('obj'));
@@ -23,9 +23,6 @@ my $idNivel3=$obj->{'id3'};
 
 my @nivel2Loop= &C4::AR::Busquedas::MARCDetail($idNivel3,'intra');
 
-$template->param(
- 	loopnivel2 => \@nivel2Loop,
-);
+$t_params->{'loopnivel2'}= \@nivel2Loop;
 
-
-output_html_with_http_headers $input, $cookie, $template->output;
+C4::Auth::output_html_with_http_headers($input, $template, $t_params, $session);
