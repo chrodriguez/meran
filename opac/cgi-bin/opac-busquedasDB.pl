e6cfd7c888b11470d35f6314e1b6a531924a16db
Miguel Carbone <mcarbone@mail.linti.unlp.edu.ar>
2009-04-22 14:41:56 +0000

    git-svn-id: svn+ssh://163.10.10.34/var/repositorioV2-V3@1345 107f0ce8-b514-0410-b978-fdcfc4524ee3

---
+++ opac/cgi-bin/opac-busquedasDB.pl
@@ -0,0 +1,70 @@
+#!/usr/bin/perl
+
+use strict;
+use CGI;
+use C4::Auth;
+use C4::Output;
+use C4::Interface::CGI::Output;
+use C4::AR::Busquedas;
+
+my $input = new CGI;
+
+my ($template, $session, $t_params)= get_template_and_user({
+																template_name => "busquedaResult.tmpl",
+																query => $input,
+																type => "opac",
+																authnotrequired => 1,
+																flagsrequired => {borrow => 1},
+			     });
+
+
+my $obj=$input->param('obj');
+
+if($obj ne ""){
+	$obj= C4::AR::Utilidades::from_json_ISO($obj);
+}
+
+my $ini= $obj->{'ini'};
+my $cantidad;
+my $resultsarray;
+$obj->{'type'} = 'OPAC';
+
+my ($ini,$pageNumber,$cantR)=C4::AR::Utilidades::InitPaginador($ini);
+
+if($obj->{'tipoAccion'} eq 'BUSQUEDA_SIMPLE_POR_AUTOR'){
+
+	$obj->{'autor'}= $obj->{'searchinc'};
+	($cantidad, $resultsarray)= C4::AR::Busquedas::busquedaSimplePorAutor($ini,$cantR,$obj,$session);
+
+}elsif($obj->{'tipoAccion'} eq 'BUSQUEDA_SIMPLE_POR_TITULO'){
+
+	$obj->{'titulo'}= $obj->{'searchinc'};
+	($cantidad, $resultsarray)= C4::AR::Busquedas::busquedaSimplePorTitulo($ini,$cantR,$obj,$session);
+
+}elsif($obj->{'tipoAccion'} eq 'BUSQUEDA_SIMPLE_POR_TEMA'){
+
+	$obj->{'tema'}= $obj->{'searchinc'};
+# FIXME falta implementar
+
+}elsif($obj->{'tipoAccion'} eq 'BUSQUEDA_COMBINABLE'){
+
+	($cantidad, $resultsarray)= C4::AR::Busquedas::busquedaAvanzada_newTemp($ini,$cantR,$obj,$session);
+}
+
+
+=item
+my ($ini,$pageNumber,$cantR)=C4::AR::Utilidades::InitPaginador($ini);
+
+my ($cantidad, $resultsarray)= C4::AR::Busquedas::busquedaAvanzada_newTemp($ini,$cantR,$obj,$session);
+
+=cut
+
+$t_params->{'paginador'} = C4::AR::Utilidades::crearPaginador($cantidad,$cantR, $pageNumber,$obj->{'funcion'},$t_params);
+#se arma el arreglo con la info para mostrar en el template
+$obj->{'cantidad'}= $cantidad;
+$obj->{'loggedinuser'}= $session->param('nro_socio');
+$t_params->{'SEARCH_RESULTS'}= $resultsarray;
+$t_params->{'buscoPor'}= C4::AR::Busquedas::armarBuscoPor($obj);
+$t_params->{'cantidad'}= $cantidad;
+
+C4::Auth::output_html_with_http_headers($input, $template, $t_params, $session);
