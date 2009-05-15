473694047ef7e491f9afe359da08ec33f6c462be
Gaspar Rajoy <grajoy@mail.linti.unlp.edu.ar>
2009-05-15 15:11:44 +0000

    git-svn-id: svn+ssh://163.10.10.34/var/repositorioV2-V3@1481 107f0ce8-b514-0410-b978-fdcfc4524ee3

--- intranet/cgi-bin/autocompletablesDB.pl
+++ intranet/cgi-bin/autocompletablesDB.pl
@@ -26,34 +26,41 @@ if ($accion eq 'autocomplete_ciudades'){
     $result = C4::AR::Utilidades::ciudadesAutocomplete($string);
 }
 elsif ($accion eq 'autocomplete_paises'){
-    
+
     $result = C4::AR::Utilidades::paisesAutocomplete($string);
 }
 elsif ($accion eq 'autocomplete_lenguajes'){
-    
+
     $result = C4::AR::Utilidades::lenguajesAutocomplete($string);
 }
 elsif ($accion eq 'autocomplete_autores'){
-    
+
     $result = C4::AR::Utilidades::autoresAutocomplete($string);
 }
 elsif ($accion eq 'autocomplete_soportes'){
-    
+
     $result = C4::AR::Utilidades::soportesAutocomplete($string);
 }
 elsif ($accion eq 'autocomplete_usuarios'){
-    
+
     $result = C4::AR::Utilidades::usuarioAutocomplete($string);
 }
 elsif ($accion eq 'autocomplete_barcodes_prestados'){
-    
+
 	$result = C4::AR::Utilidades::barcodePrestadoAutocomplete($string);
 }
 elsif ($accion eq 'autocomplete_barcodes'){
-    
+
      $result = C4::AR::Utilidades::barcodeAutocomplete($string);
 }
+elsif ($accion eq 'autocomplete_temas'){
 
+     $result = C4::AR::Utilidades::autocompleteTemas($string);
+}
+elsif ($accion eq 'autocomplete_editoriales'){
+
+     $result = C4::AR::Utilidades::autocompleteEditoriales($string);
+}
 C4::Output::printHeader($session);
 print $result;
 
