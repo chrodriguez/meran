0ebad97e93b3ad9769576bbfb4eeaa068e1f8dee
Miguel Carbone <mcarbone@mail.linti.unlp.edu.ar>
2009-06-16 20:02:21 +0000

    git-svn-id: svn+ssh://163.10.10.34/var/repositorioV2-V3@1620 107f0ce8-b514-0410-b978-fdcfc4524ee3

--- intranet/modules/C4/Modelo/UsrSocio.pm
+++ intranet/modules/C4/Modelo/UsrSocio.pm
@@ -503,7 +503,7 @@ sub tienePermisos {
 
     $self->log($flagsrequired,'tienePermisos => permisos requeridos');
     #Obtengo los permisos del socio
-=item
+
     my $flags= $self->getPermisos;
 
     if($flagsrequired){
@@ -519,7 +519,7 @@ sub tienePermisos {
 
 
     return 0;
-=cut
+
 
 =item
      {   tipo_documento => $tipo_documento, 
@@ -531,17 +531,17 @@ sub tienePermisos {
 #     00000000 8bits los primeros 4 esta para uso futuro
 #     000TABMC TODOS, ALTA, BAJA,MODIFICACION, CONSULTA 
 
-    my $flags= $self->getPermisos;
-
-    $flagsrequired->{'tipo_documento'}
-    $flagsrequired->{'accion'}
-    $flagsrequired->{'ui'}
-    $flagsrequired->{'nivel'} #datos_nivel3 | datos_nivel2 | datos_nivel1
-    get_permiso({   nivel => $flagsrequired->{'nivel'}, 
-                    ui => $flagsrequired->{'ui'}, 
-                    tipo_documento => $flagsrequired->{'tipo_documento'}, 
-                    id_persona => $self->getId_persona }
-                );
+#     my $flags= $self->getPermisos;
+# 
+#     $flagsrequired->{'tipo_documento'}
+#     $flagsrequired->{'accion'}
+#     $flagsrequired->{'ui'}
+#     $flagsrequired->{'nivel'} #datos_nivel3 | datos_nivel2 | datos_nivel1
+#     get_permiso({   nivel => $flagsrequired->{'nivel'}, 
+#                     ui => $flagsrequired->{'ui'}, 
+#                     tipo_documento => $flagsrequired->{'tipo_documento'}, 
+#                     id_persona => $self->getId_persona }
+#                 );
 
     
 
