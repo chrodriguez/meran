c19513985cb16e00116896db50a9e912f9084191
Miguel Carbone <mcarbone@mail.linti.unlp.edu.ar>
2009-08-18 19:21:45 +0000

    git-svn-id: svn+ssh://163.10.10.34/var/repositorioV2-V3@1906 107f0ce8-b514-0410-b978-fdcfc4524ee3

--- intranet/cgi-bin/mainpage.pl
+++ intranet/cgi-bin/mainpage.pl
@@ -9,19 +9,14 @@ use CGI;
 
 my $query = new CGI;
 
-my ($template, $session, $params)= get_template_and_user({
+my ($template, $session, $t_params)= get_template_and_user({
 									template_name => "main.tmpl",
 									query => $query,
 									type => "intranet",
 									authnotrequired => 0,
-# 									flagsrequired => {catalogue => 1, circulate => 1,
                                     flagsrequired => { ui => 'ANY', tipo_documento => 'ANY', accion => 'CONSULTA', entorno => 'undefined'},
-# 									parameters => 1, borrowers => 1,
-# 									permissions =>1, reserveforothers=>1,
-# 									borrow => 1, reserveforself => 1,
-# 									editcatalogue => 1, updatesanctions => 1, },
 									debug => 1,
 			});
 
 
-C4::Auth::output_html_with_http_headers($query, $template, $params,$session);
+C4::Auth::output_html_with_http_headers($query, $template, $t_params,$session);
