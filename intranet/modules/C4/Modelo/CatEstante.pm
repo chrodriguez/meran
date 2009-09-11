47925807cc8eb04833a2e0f06b98e3085dbfbca9
Matias Pagano <matiasp@mail.linti.unlp.edu.ar>
2009-09-11 20:15:56 +0000

    search and destroy
    git-svn-id: svn+ssh://163.10.10.34/var/repositorioV2-V3@2110 107f0ce8-b514-0410-b978-fdcfc4524ee3

--- intranet/modules/C4/Modelo/CatEstante.pm
+++ intranet/modules/C4/Modelo/CatEstante.pm
@@ -8,14 +8,27 @@ __PACKAGE__->meta->setup(
     table   => 'cat_estante',
 
     columns => [
-        shelfnumber => { type => 'integer', not_null => 1 },
-        shelfname   => { type => 'varchar', length => 255 },
-        type        => { type => 'text', length => 65535, not_null => 1 },
-        parent      => { type => 'integer', default => '0', not_null => 1 },
+        id        => { type => 'integer', not_null => 1 },
+        estante   => { type => 'varchar', length => 255 },
+        tipo      => { type => 'text', length => 65535, not_null => 1 },
+        padre     => { type => 'integer', default => '0', not_null => 1 },
     ],
 
-    primary_key_columns => [ 'shelfnumber' ],
+    primary_key_columns => [ 'id' ],
 );
+
+
+sub getId{
+    my ($self) = shift;
+    return ($self->id);
+}
+
+sub setId{
+    my ($self) = shift;
+    my ($id) = @_;
+    $self->id($id);
+}
+
 
 1;
 
