62b3f0668fecd39cac56af37ee605741e224451c
Miguel Carbone <mcarbone@mail.linti.unlp.edu.ar>
2009-09-11 19:38:31 +0000

    Impiratciones y Exportaciones para TUTO q lo mira por TV
    git-svn-id: svn+ssh://163.10.10.34/var/repositorioV2-V3@2107 107f0ce8-b514-0410-b978-fdcfc4524ee3

---
+++ intranet/cgi-bin/herramientas/importacion.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+use HTML::Template;
+use strict;
+require Exporter;
+
+use C4::Output;  # contains gettemplate
+use C4::Auth;
+use CGI;
+
+my $query = new CGI;
+
+my ($template, $session, $t_params)= get_template_and_user({
+									template_name => "main.tmpl",
+									query => $query,
+									type => "intranet",
+									authnotrequired => 0,
+                                    flagsrequired => { ui => 'ANY', tipo_documento => 'ANY', accion => 'CONSULTA', entorno => 'undefined'},
+									debug => 1,
+			});
+
+
+C4::Auth::output_html_with_http_headers($template, $t_params,$session);
