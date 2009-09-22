591c40c4feffe6e7edc01565a7481a286926bb09
Gaspar Rajoy <grajoy@mail.linti.unlp.edu.ar>
2009-09-22 15:34:43 +0000

    TABLAS DE REFERENCIA READY FOR TESTING :)
    git-svn-id: svn+ssh://163.10.10.34/var/repositorioV2-V3@2168 107f0ce8-b514-0410-b978-fdcfc4524ee3

--- intranet/modules/C4/Modelo/RefLocalidad.pm
+++ intranet/modules/C4/Modelo/RefLocalidad.pm
@@ -102,12 +102,39 @@ sub lastTable{
 sub getAll{
 
     my ($self) = shift;
-    my ($limit,$offset)=@_;
+    my ($limit,$offset,$matchig_or_not)=@_;
     use C4::Modelo::RefLocalidad::Manager;
-    
-    my $ref_valores = C4::Modelo::RefLocalidad::Manager->get_ref_localidad( limit => $limit, offset => $offset);
-        
-    return ($ref_valores);
+    use Text::LevenshteinXS;
+    $matchig_or_not = $matchig_or_not || 0;
+    my @filtros;
+
+    push(@filtros, ($self->getPk => {ne => $self->getPkValue}) );
+    my $ref_valores;
+    if ($matchig_or_not){ #ESTOY BUSCANDO SIMILARES, POR LO TANTO NO TENGO QUE LIMITAR PARA PERDER RESULTADOS
+        $ref_valores = C4::Modelo::RefLocalidad::Manager->get_ref_localidad(query => \@filtros,);
+    }else{
+        $ref_valores = C4::Modelo::RefLocalidad::Manager->get_ref_localidad(query => \@filtros,
+                                                                    limit => $limit, 
+                                                                    offset => $offset, 
+                                                                    sort_by => ['nombre'] 
+                                                                   );
+    }
+    my $self_nombre = $self->getNombre;
+
+    my $match = 0;
+    if ($matchig_or_not){
+        my @matched_array;
+        foreach my $each (@$ref_valores){
+          $match = ((distance($self_nombre,$each->getNombre)<=1));
+          if ($match){
+            push (@matched_array,$each);
+          }
+        }
+        return (\@matched_array);
+    }
+    else{
+      return($ref_valores);
+    }
 }
 
 1;
