7183402f01fd5036f9356b1ed7cf95db90907c3d
Gaspar Rajoy <grajoy@mail.linti.unlp.edu.ar>
2009-11-16 20:00:59 +0000

    git-svn-id: svn+ssh://163.10.10.34/var/repositorioV2-V3@2315 107f0ce8-b514-0410-b978-fdcfc4524ee3

--- intranet/modules/C4/Auth.pm
+++ intranet/modules/C4/Auth.pm
@@ -468,7 +468,7 @@ sub checkauth {
     my $loggedin = 0;
     my %info;
     my ($session) = CGI::Session->load();
-
+C4::AR::Debug::debug("checkauth=> DUMP DESPUES DE LOAD SESSION: ".$session->dump());
     my ($userid, $cookie, $sessionID, $flags);
 
     #verifica que no haya sesiones "colgadas", las borra de la base
@@ -493,7 +493,7 @@ sub checkauth {
         #se esta intentando levantar un ID de session que no existe en la BD, puede ser el caso en que se haya borrado
         #de la base
             $sessionID = undef;
-            $userid = undef;    
+            $userid = undef;
             _destruirSession('U406');
         }
 
--- opac/cgi-bin/logout.pl
+++ opac/cgi-bin/logout.pl
@@ -1,75 +1,23 @@
 #!/usr/bin/perl
 
+use strict;
+require Exporter;
 
-# Copyright 2000-2002 Katipo Communications
-#
-# This file is part of Koha.
-#
-# Koha is free software; you can redistribute it and/or modify it under the
-# terms of the GNU General Public License as published by the Free Software
-# Foundation; either version 2 of the License, or (at your option) any later
-# version.
-#
-# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along with
-# Koha; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
-# Suite 330, Boston, MA  02111-1307 USA
-
-use CGI;
+use C4::Output;  # contains gettemplate
+use C4::Auth;
 use C4::Context;
+use CGI::Session;
 
-my $query=new CGI;
+my $input=new CGI;
 
-my $sessionID=$query->cookie('sessionID');
+my ($template, $session, $t_params) =  C4::Auth::get_template_and_user ({
+            template_name   => 'auth.tmpl',
+            query       => $input,
+            type        => "intranet",
+            authnotrequired => 0,
+            flagsrequired   => { ui => 'ANY', tipo_documento => 'ANY', accion => 'CONSULTA', entorno => 'undefined'},
+    });
 
-my $sessions;
-open (S, "/tmp/sessions");
-while (my ($sid, $u, $lasttime) = split(/:/, <S>)) {
-    chomp $lasttime;
-    (next) unless ($sid);
-    (next) if ($sid eq $sessionID);
-    $sessions->{$sid}->{'userid'}=$u;
-    $sessions->{$sid}->{'lasttime'}=$lasttime;
-}
-open (S, ">/tmp/sessions");
-foreach (keys %$sessions) {
-    my $userid=$sessions->{$_}->{'userid'};
-    my $lasttime=$sessions->{$_}->{'lasttime'};
-    print S "$_:$userid:$lasttime\n";
-}
+my ($session)= C4::Auth::cerrarSesion();
 
-my $dbh = C4::Context->dbh;
-
-# Check that this is the ip that created the session before deleting it
-
-my $sth=$dbh->prepare("select userid,ip from sist_sesion where sessionID=?");
-$sth->execute($sessionID);
-my ($userid, $ip);
-if ($sth->rows) {
-    ($userid,$ip) = $sth->fetchrow;
-    if ($ip ne $ENV{'REMOTE_ADDR'}) {
-       # attempt to logout from a different ip than cookie was created at
-       exit;
-    }
-}
-
-$sth=$dbh->prepare("delete from sist_sesion where sessionID=?");
-$sth->execute($sessionID);
-open L, ">>/tmp/sessionlog";
-my $time=localtime(time());
-printf L "%20s from %16s logged out at %30s (manual log out).\n", $userid, $ip, $time;
-close L;
-
-my $cookie=$query->cookie(-name => 'sessionID',
-			  -value => '',
-			  -expires => '+1y');
-
-# Should redirect to intranet home page after logging out
-
-print $query->redirect("userpage.pl");
-exit;
-
-
+C4::Auth::output_html_with_http_headers($template, $t_params, $session);
