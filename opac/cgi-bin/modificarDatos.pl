5cbb780d6db69fd799e5c54e2ccd83bb5d1bca25
Gaspar Rajoy <grajoy@mail.linti.unlp.edu.ar>
2009-11-23 19:34:12 +0000

    OPAC (The Making Of)
    git-svn-id: svn+ssh://163.10.10.34/var/repositorioV2-V3@2339 107f0ce8-b514-0410-b978-fdcfc4524ee3

--- opac/cgi-bin/modificarDatos.pl
+++ opac/cgi-bin/modificarDatos.pl
@@ -13,7 +13,7 @@ my $input=new CGI;
 my ($template, $session, $t_params) =  C4::Auth::get_template_and_user ({
             template_name   => 'opac-main.tmpl',
             query       => $input,
-            type        => "intranet",
+            type        => "opac",
             authnotrequired => 0,
             flagsrequired   => { ui => 'ANY', tipo_documento => 'ANY', accion => 'CONSULTA', entorno => 'undefined'},
     });
