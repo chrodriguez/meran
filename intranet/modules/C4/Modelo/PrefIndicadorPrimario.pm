703d56020f34c1b7c465932522300a910cec7da5
Miguel Carbone <mcarbone@mail.linti.unlp.edu.ar>
2009-11-26 16:25:09 +0000

    git-svn-id: svn+ssh://163.10.10.34/var/repositorioV2-V3@2351 107f0ce8-b514-0410-b978-fdcfc4524ee3

---
+++ intranet/modules/C4/Modelo/PrefIndicadorPrimario.pm
@@ -0,0 +1,74 @@
+package C4::Modelo::PrefIndicadorPrimario;
+
+use strict;
+
+use base qw(C4::Modelo::DB::Object::AutoBase2);
+
+__PACKAGE__->meta->setup(
+    table   => 'pref_indicador_primario',
+
+    columns => [
+        id              => { type => 'serial', not_null => 1 },
+        indicador       => { type => 'character', default => '', length => 255, not_null => 1 },
+        dato            => { type => 'character', default => '', length => 255, not_null => 1 },
+        campo_marc      => { type => 'character', default => '', length => 3, not_null => 1 },
+    ],
+
+    primary_key_columns => [ 'id' ],
+
+	relationships => [
+	    ref_pref_estructura_campo_marc => {
+            class      => 'C4::Modelo::PrefEstructuraCampoMarc',
+            column_map => { campo_marc => 'campo' },
+            type       => 'one to one',
+        },
+    ],
+
+);
+
+
+
+
+sub getCampo{
+    my ($self) = shift;
+    return ($self->campo_marc);
+}
+
+sub setCampo{
+    my ($self) = shift;
+    my ($campo_marc) = @_;
+    $self->campo_marc($campo_marc);
+}
+
+sub getId{
+    my ($self) = shift;
+    return ($self->id);
+}
+
+sub getIndicador{
+    my ($self) = shift;
+    return ($self->indicador);
+}
+
+sub setIndicador{
+    my ($self) = shift;
+    my ($indicador) = @_;
+    $self->indicador($indicador);
+}
+
+sub getDato{
+    my ($self) = shift;
+    return ($self->dato);
+}
+
+sub setDato{
+    my ($self) = shift;
+    my ($dato) = @_;
+    $self->dato($dato);
+}
+
+
+
+
+1;
+
