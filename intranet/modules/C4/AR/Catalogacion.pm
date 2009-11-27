3e227e9b4f95d99682996569655151cda8e9f3fd
Einar Lanfranco <einar@mail.info.unlp.edu.ar>
2009-11-27 20:47:32 +0000

    git-svn-id: svn+ssh://163.10.10.34/var/repositorioV2-V3@2381 107f0ce8-b514-0410-b978-fdcfc4524ee3

--- intranet/modules/C4/AR/Catalogacion.pm
+++ intranet/modules/C4/AR/Catalogacion.pm
@@ -64,60 +64,11 @@ sub meran_nivel1_to_meran{
     foreach my $infoNivel1 (@$infoArrayNivel1){
         my $field = MARC::Field->new($infoNivel1->{'campo'}, $infoNivel1->{'indetificador_1'}, $infoNivel1->{'indetificador_2'});
         foreach my $subcampo (@$infoNivel1->{'subcampos'}){
-                $field->add_subfield($subcampo->{'subcampo'},$subcampo->{'dato'})
+                $field->add_subfield($subcampo->{'subcampo'},$subcampo->{'dato'});
             }
     }
     return($marc_record);
     }
-
-    
-#     $self->save();
-# 
-#     my $id1 = $self->getId1;
-# 
-#     #Se guradan los datos en Nivel 1 repetibles
-#     foreach my $infoNivel1 (@arrayNivel1Repetibles){
-#         $infoNivel1->{'id1'} = $id1;
-# # SI NO EXISTE EL rep_n1_id hay q crear una tupla, puede q sea un registro importado que no tenia este dato
-#         my $nivel1Repetible;
-# 
-#         C4::AR::Debug::debug("CatNivel1 => campo, subcampo: ".$infoNivel1->{'campo'}.", ".$infoNivel1->{'subcampo'});
-# 
-#         if ( $infoNivel1->{'Id_rep'} != 0 ){
-#             C4::AR::Debug::debug("CatNivel1 => agregar => Se va a modificar CatNivel1, Id_rep: ". $infoNivel1->{'Id_rep'});
-# # getNivel2RepetibleFromId2Repetible
-# #             $nivel1Repetible = C4::Modelo::CatNivel1Repetible->new(db => $self->db, rep_n1_id => $infoNivel1->{'Id_rep'});
-# #             $nivel1Repetible->load();
-#             $nivel1Repetible = C4::AR::Nivel1::getNivel1RepetibleFromId1Repetible($infoNivel1->{'Id_rep'},$self->db);
-#         }else{
-#             C4::AR::Debug::debug("CatNivel1 => agregar => No existe el REPETIBLE se crea uno");
-#             $nivel1Repetible = C4::Modelo::CatNivel1Repetible->new(db => $self->db);
-#         }
-# 
-#         $nivel1Repetible->setId1($infoNivel1->{'id1'});
-#         $nivel1Repetible->setCampo($infoNivel1->{'campo'});
-#         $nivel1Repetible->setSubcampo($infoNivel1->{'subcampo'});
-# 
-#         if ($infoNivel1->{'referencia'}) {
-#             C4::AR::Debug::debug("CatNivel1 => REPETIBLE con REFERENCIA: ".$infoNivel1->{'datoReferencia'});
-#             $nivel1Repetible->dato($infoNivel1->{'datoReferencia'});
-#         }else{
-#             $nivel1Repetible->dato($infoNivel1->{'dato'});
-#             C4::AR::Debug::debug("CatNivel1 => REPETIBLE sin REFERENCIA: ".$infoNivel1->{'dato'});
-#         }
-# 
-#         $nivel1Repetible->save(); 
-#     }
-
-
-
-
-
-
-
-
-
-}
 
 =head2
 sub meran_nivel2_to_meran
