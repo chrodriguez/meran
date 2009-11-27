e2e1a27b1f3c2b7687797a61c9b3de0bfe0ecd13
Matias Pagano <matiasp@mail.linti.unlp.edu.ar>
2009-11-27 21:46:31 +0000

    indice
    git-svn-id: svn+ssh://163.10.10.34/var/repositorioV2-V3@2387 107f0ce8-b514-0410-b978-fdcfc4524ee3

--- intranet/scripts/generar_indice_v2.pl
+++ intranet/scripts/generar_indice_v2.pl
@@ -21,7 +21,7 @@ while (my $registro_marc_n1 = $sth1->fetchrow_hashref ){
     C4::AR::Debug::debug('ID1 '.$registro_marc_n1->{'id'});
     my $marc_record = MARC::Record->new_from_usmarc($registro_marc_n1->{'marc_record'});
 
-    my $query2=" SELECT * FROM cat_registro_marc_n2 where id1 = ? ";
+    my $query2=" SELECT * FROM cat_registro_marc_n2 where id1 =?;";
     my $sth2=$dbh->prepare($query2);
     $sth2->execute($registro_marc_n1->{'id'});
 
@@ -31,7 +31,7 @@ while (my $registro_marc_n1 = $sth1->fetchrow_hashref ){
         $marc_record->add_fields($marc_record2->fields);
 
 
-        my $query3=" SELECT * FROM cat_registro_marc_n3 where id1=? and id2 = ?";
+        my $query3=" SELECT * FROM cat_registro_marc_n3 where id1=? and id2=?;";
         my $sth3=$dbh->prepare($query3);
         $sth3->execute($registro_marc_n1->{'id'},$registro_marc_n2->{'id'});
 
@@ -45,6 +45,9 @@ while (my $registro_marc_n1 = $sth1->fetchrow_hashref ){
 #Ahora en $marc_record tenemos todo el registro completo
     #Autor
     my $autor = $marc_record->subfield("100","a");
+        if(! $autor) { $autor = $marc_record->subfield("110","a");}
+        if(! $autor) { $autor = $marc_record->subfield("111","a");}
+
     #Titulo
     my $titulo = $marc_record->subfield("245","a");
 
@@ -54,12 +57,12 @@ while (my $registro_marc_n1 = $sth1->fetchrow_hashref ){
    foreach my $field ($marc_record->fields){
 
             if ($superstring eq "") {$superstring =$field->as_string; }
-                else {$superstring .=" ".@$field->as_string; }
+                else {$superstring .=" ".$field->as_string; }
     }
 
-    my $query4="INSERT INTO indice_busqueda (id,titulo,autor,string,registro_marc) VALUES (?,?,?,?,?) ";
+    my $query4="INSERT INTO indice_busqueda (id,titulo,autor,string) VALUES (?,?,?,?) ";
     my $sth4=$dbh->prepare($query4);
-    $sth4->execute($registro_marc_n1->{'id'},$titulo,$autor,$superstring,$marc_record->as_usmarc);
+    $sth4->execute($registro_marc_n1->{'id'},$titulo,$autor,$superstring);
 
 
 }
