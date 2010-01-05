41ca7fc6e49f233662be19c82018c714819b7255
Gaspar Rajoy <grajoy@mail.linti.unlp.edu.ar>
2010-01-05 17:47:03 +0000

    Visualizacion De la Intra
    git-svn-id: svn+ssh://163.10.10.34/var/repositorioV2-V3@2601 107f0ce8-b514-0410-b978-fdcfc4524ee3

---
+++ intranet/modules/C4/AR/VisualizacionIntra.pm
@@ -0,0 +1,125 @@
+package C4::AR::VisualizacionOpac;
+
+#
+#Este modulo sera el encargado del manejo de la carga de datos en las tablas MARC
+#Tambien en la carga de los items en los distintos niveles.
+#
+
+use strict;
+require Exporter;
+use C4::Context;
+use C4::Modelo::CatVisualizacionOpac;
+use C4::Modelo::CatVisualizacionOpac::Manager;
+use C4::Modelo::CatEstructuraCatalogacion;
+use C4::Modelo::CatEstructuraCatalogacion::Manager;
+use vars qw($VERSION @EXPORT @ISA);
+
+# set the version for version checking
+$VERSION = 0.01;
+
+@ISA=qw(Exporter);
+
+@EXPORT=qw(
+
+	&getConfiguracion
+    &deleteConfiguracion
+
+);
+
+
+sub getConfiguracion{
+    my ($perfil) = @_;
+    my @filtros;
+
+    $perfil = $perfil || C4::AR::Preferencias->getValorPreferencia('perfil_opac');
+
+    push ( @filtros, ( or   => [    id_perfil   => { eq => $id_perfil }, 
+                                    id_perfil   => { eq => '0'     } ]) #PERFIL TODOS
+                );
+
+    my $configuracion = C4::Modelo::CatVisualizacionOpac::Manager->get_cat_visualizacion_opac(query => \@filtros,);
+
+    return ($configuracion);
+}
+
+sub addConfiguracion{
+    my ($params) = @_;
+    my @filtros;
+
+    my $configuracion = C4::Modelo::CatVisualizacionOpac->new();
+
+    $configuracion->agregar($params);
+    
+    return ($configuracion);
+}
+
+sub deleteConfiguracion{
+    my ($params) = @_;
+    my @filtros;
+    my $vista_id = $params->{'vista_id'};
+
+    push (@filtros, (id => { eq => $vista_id }) );
+    my $configuracion = C4::Modelo::CatVisualizacionOpac::Manager->get_cat_visualizacion_opac(query => \@filtros,);
+
+    if ($configuracion->[0]){
+        return ( $configuracion->[0]->delete() );
+    }else{
+        return(0);
+    }
+}
+
+sub editConfiguracion{
+    my ($vista_id,$value) = @_;
+    my @filtros;
+
+    push (@filtros, (id => { eq => $vista_id }) );
+    my $configuracion = C4::Modelo::CatVisualizacionOpac::Manager->get_cat_visualizacion_opac(query => \@filtros,);
+
+    if ($configuracion->[0]){
+        $configuracion->[0]->modificar($value);
+        return ( $configuracion->[0]->getVistaOpac() );
+    }else{
+        return(0);
+    }
+}
+
+=head2 sub getSubCamposLike
+    Obtiene los subcampos haciendo busqueda like, para el nivel indicado
+=cut
+sub getSubCamposLike{
+    my ($campo) = @_;
+
+    my @filtros;
+
+    push(@filtros, ( campo => { eq => $campo} ) );
+
+    my $db_campos_MARC = C4::Modelo::CatEstructuraCatalogacion::Manager->get_cat_estructura_catalogacion(
+                                                                query => \@filtros,
+                                                                sort_by => ('subcampo'),
+                                                                select   => [ 'subcampo', 'liblibrarian', 'obligatorio' ],
+                                                                group_by => [ 'subcampo'],
+                                                            );
+    return($db_campos_MARC);
+}
+
+=head2 sub getCamposXLike
+    Busca un campo like..., segun nivel indicado
+=cut
+sub getCamposXLike{
+    my ($campoX) = @_;
+
+    my @filtros;
+
+    push(@filtros, ( campo => { like => $campoX.'%'} ) );
+
+    my $db_campos_MARC = C4::Modelo::CatEstructuraCatalogacion::Manager->get_cat_estructura_catalogacion(
+                                                                                        query => \@filtros,
+                                                                                        sort_by => ('campo'),
+                                                                                        select   => [ 'campo', 'liblibrarian'],
+                                                                                        group_by => [ 'campo'],
+                                                                       );
+    return($db_campos_MARC);
+}
+
+
+1;
