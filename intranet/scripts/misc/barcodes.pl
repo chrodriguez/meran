430daaabbb680811d57ca5dee9e973ef55e2c372
matiasp <matiasp@mail.linti.unlp.edu.ar>
2010-02-04 11:22:19 -0300

    Revert "test 2"
    This reverts commit fb100a60e5f4995ed12d66383e4839dfb16817f1.

--- intranet/scripts/misc/barcodes.pl
+++ intranet/scripts/misc/barcodes.pl
@@ -7,6 +7,7 @@
 ####################
 
 # Copyright 2000-2002 Katipo Communications
+# NANANA
 # This file is part of Koha.
 #
 # Koha is free software; you can redistribute it and/or modify it under the
