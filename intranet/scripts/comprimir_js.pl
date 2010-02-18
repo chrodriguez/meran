4d6c967456286f0c310cf1ea4ec780ae769015aa
Testing2030 <root@meran.linti.unlp.edu.ar>
2010-02-18 19:50:08 -0300

    speedup

---
+++ intranet/scripts/comprimir_js.pl
@@ -0,0 +1,37 @@
+use JavaScript::Minifier qw(minify);
+
+#procesa el archivo archivos_a_comprimir.txt y comprime los js
+my $path="/usr/local/koha";
+open (FILE, "/usr/local/koha/intranet/scripts/archivos_a_comprimir_intranet_top.txt" ) or die "No se pudo abrir el archivo: $!";
+system ("rm $path/includes/completo-intranet-top.js");
+while ( <FILE> ) {
+    
+    my($line) = $_;
+    chomp($line);
+    open(INFILE, $path.$line.'.js') or die;
+    open(OUTFILE, '>'.$path.$line.'-min.js') or die $line ;
+    minify(input => *INFILE, outfile => *OUTFILE);
+    close(INFILE);
+    close(OUTFILE);
+    system ("cat ".$path.$line."-min.js >> $path/includes/completo-intranet-top.js");
+}
+#open(INFILE, 'myScript.js') or die;
+#open(OUTFILE, '>myScript-min.js') or die;
+
+close(FILE);
+
+#procesa el archivo archivos_a_comprimir_individuales.txt y comprime los js en archivos -min
+open (FILE, "/usr/local/koha/intranet/scripts/archivos_a_comprimir_individuales.txt" ) or die "No se pudo abrir el archivo: $!";
+while ( <FILE> ) {
+    my($line) = $_;
+    chomp($line);
+    open(INFILE, $line.'.js') or die $line;
+    open(OUTFILE, '>'.$line.'-min.js') or die $line ;
+    minify(input => *INFILE, outfile => *OUTFILE);
+    close(INFILE);
+    close(OUTFILE);
+}
+#open(INFILE, 'myScript.js') or die;
+#open(OUTFILE, '>myScript-min.js') or die;
+
+close(FILE);
