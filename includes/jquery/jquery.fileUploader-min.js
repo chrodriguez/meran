f3c63ca2ced4dbf5d14cb7dd0441b0fab8f6813f
Testing2030 <root@meran.linti.unlp.edu.ar>
2010-02-19 10:53:32 -0300

    packed

---
+++ includes/jquery/jquery.fileUploader-min.js
@@ -0,0 +1,13 @@
+var fake_file="fake_file";function assignFileName(idInput){$("#"+idInput).val($("#myUploadFile").val());};(function($){$.fn.extend({fileUploader:function(options){opt=$.extend({},$.uploadSetUp.defaults,options);if(opt.file_types.match('jpg')&&!opt.file_types.match('jpeg'))
+opt.file_types+=',jpeg';$this=$(this);new $.uploadSetUp();}});$.uploadSetUp=function(){$('body').append($('<div></div>').append($('<iframe src="about:blank" id="myFrame" name="myFrame" style="display: none;"></iframe>')));$this.append($('<form target="myFrame" enctype="multipart/form-data" action="'+opt.ajaxFile+'" method="post" name="myUploadForm" id="myUploadForm"></form>')
+.append($('<input type="hidden" name="nro_socio" value="'+opt.nro_socio+'" />'),$('<input type="hidden" name="upload" value="'+opt.uploadFolder+'" />'),$('<div class="select" title="Subir una foto"></div>').append($('<div class="fileinputs">'+
+'<input id="myUploadFile" class="file" type="file" value="" name="picture"/>'+
+'<div class="fakefile">'+
+'<input id="fake_file"/>'+
+'<img src='+imagesForJS+"/iconos/anadir.png"+' />'+
+'</div>'+
+'</div>')),$('<ul id="ul_files"></ul>')));init();};$.uploadSetUp.defaults={file_types:"jpg,gif,png",ajaxFile:"upload.pl",uploadFolder:"/ajaxMultiFileUpload/upload/",funcionOnComplete:'',};function init(){$('#myUploadFile').livequery('change',function(){if(checkFileType(this.value))
+$('#myUploadForm').submit();});$('#myUploadForm').submit(function(){return event.submit(this);});var event={frame:function(_form){$("#myFrame")
+.empty()
+.one('load',function(){event.loaded(this,_form)});},submit:function(_form){assignFileName(fake_file);$('.select').addClass('waiting');event.frame(_form);},loaded:function(){if(opt.funcionOnComplete){opt.funcionOnComplete();}}};function checkFileType(file_){var ext_=file_.toLowerCase().substr(file_.toLowerCase().lastIndexOf('.')+1);if(!opt.file_types.match(ext_)){alert('tipo de archivo '+ext_+' no permitido');return false;}
+else return true;};function frametype(fid){return(fid.contentDocument)?fid.contentDocument:(fid.contentWindow)?fid.contentWindow.document:window.frames[fid].document;};}})(jQuery);
