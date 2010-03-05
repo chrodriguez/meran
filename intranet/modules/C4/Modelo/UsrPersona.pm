27912b2689f555162fd372fdf7c1394ebf9413a3
Gaspar Rajoy <grajoy@linti.unlp.edu.ar>
2010-03-05 08:53:54 -0300

    Fixed bug #632 :):):):):):):)

--- intranet/modules/C4/Modelo/UsrPersona.pm
+++ intranet/modules/C4/Modelo/UsrPersona.pm
@@ -201,8 +201,7 @@ sub modificarVisibilidadOPAC{
     my ($self)=shift;
     my ($data_hash)=@_;
     #Asignando data...
-    use HTML::Entities;
-    $self->setNombre(encode_entities$data_hash->{'nombre'});
+    $self->setNombre($data_hash->{'nombre'});
     $self->setApellido($data_hash->{'apellido'});
     $self->setCalle($data_hash->{'direccion'});
     $self->setCiudad($data_hash->{'id_ciudad'});
