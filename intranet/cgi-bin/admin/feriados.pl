2848be5aa86039d3648ace15cc8c4cfdce5755f3
Gaspar Rajoy <grajoy@linti.unlp.edu.ar>
2010-03-12 09:20:54 -0300

    feriados v. PRE-ALPHA

---
+++ intranet/cgi-bin/admin/feriados.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+use strict;
+use C4::Auth;
+use CGI;
+use C4::AR::Novedades;
+my $input = new CGI;
+
+my ($template, $session, $t_params) = get_template_and_user({
+									template_name => "admin/feriados.tmpl",
+									query => $input,
+									type => "intranet",
+									authnotrequired => 0,
+									flagsrequired => { ui => 'ANY', tipo_documento => 'ANY', accion => 'CONSULTA', entorno => 'usuarios'},
+									debug => 1,
+			    });
+
+
+
+
+C4::Auth::output_html_with_http_headers($template, $t_params, $session);
