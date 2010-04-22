4e1161b206ba2c0551b8e7a6e8666b6c0b9e3c5c
Gaspar Rajoy <grajoy@linti.unlp.edu.ar>
2010-04-22 11:24:09 -0300

    minor bugs

--- intranet/modules/C4/AR/Utilidades.pm
+++ intranet/modules/C4/AR/Utilidades.pm
@@ -239,7 +239,6 @@ sub generarComboDeCredentials{
     $select_credentials{'librarian'} = 'librarian';
     $select_credentials{'superLibrarian'} = 'superLibrarian';
     my $socio = C4::AR::Usuarios::getSocioInfoPorNroSocio($params->{'nro_socio'});
-C4::AR::Debug::debug("NRO_SOCIO PARA MODIFICAR USUARIO: ".$params->{'nro_socio'}." Y CREDENCIAL: ".$socio->getCredentialType);
 
     my $default_credential = 'estudiante';
     if ($socio){
