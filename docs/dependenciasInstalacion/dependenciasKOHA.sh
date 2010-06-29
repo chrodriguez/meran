d0d700519fb3f2bf4fe44e385a6acaeb29bc60b8
Gaspar Rajoy <grajoy@linti.unlp.edu.ar>
2010-06-29 16:38:34 -0300

    Haciendo HTML to PDF

--- docs/dependenciasInstalacion/dependenciasKOHA.sh
+++ docs/dependenciasInstalacion/dependenciasKOHA.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 apt-get update -y
-apt-get install libpdf-report-perl libhtml-template-expr-perl  libhtml-template-perl libnet-ldap-perl libdbd-mysql-perl libmail-sendmail-perl libmarc-record-perl libgd-gd2-perl libarchive-zip-perl libapache2-mod-perl2 libapache-db-perl libdate-manip-perl libooolib-perl libpdf-report-perl  libchart-perl libtemplate-perl libnet-amazon-perl libnet-z3950-zoom-perl  aspell-es libtext-aspell-perl libnet-smtp-ssl-perl libspreadsheet-writeexcel-perl -y 
+apt-get install libpdf-report-perl libhtml-template-expr-perl  libhtml-template-perl libnet-ldap-perl libdbd-mysql-perl libmail-sendmail-perl libmarc-record-perl libgd-gd2-perl libarchive-zip-perl libapache2-mod-perl2 libapache-db-perl libdate-manip-perl libooolib-perl libpdf-report-perl  libchart-perl libtemplate-perl libnet-amazon-perl libnet-z3950-zoom-perl  aspell-es libtext-aspell-perl libnet-smtp-ssl-perl libspreadsheet-writeexcel-perl htmldoc -y 
 
 cpan -i JSON
 cpan -i JSON::XS
@@ -23,4 +23,4 @@ cpan -i Net::SMTP::TLS
 cpan -i Net::SSLeay
 cpan -i Net::Twitter
 cpan -i WWW::Facebook::API
-
+cpan -i HTML::HTMLDoc
