6ee0d09286b53417f515d212d4dd38bfbdf0ace1
Diego Vilches <diego@pale.linti.unlp.edu.ar>
2010-11-05 16:33:26 -0300

    Desago una prueba

--- intranet/modules/C4/AR/Busquedas.pm
+++ intranet/modules/C4/AR/Busquedas.pm
@@ -17,8 +17,6 @@ package C4::AR::Busquedas;
 #along with this program; if not, write to the Free Software
 #Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 
-#prueba 
-
 use strict;
 require Exporter;
 use C4::Context;
