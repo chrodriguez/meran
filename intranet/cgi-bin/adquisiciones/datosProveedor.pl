1b6d9338c8ea3df214c8b264656096c9295e0a30
Francisco Raposeiras <root@dev.redes>
2010-11-17 09:53:24 -0300

    sdf

--- intranet/cgi-bin/adquisiciones/datosProveedor.pl
+++ intranet/cgi-bin/adquisiciones/datosProveedor.pl
@@ -15,17 +15,11 @@ my ($template, $session, $t_params) =  C4::Auth::get_template_and_user ({
             flagsrequired   => { ui => 'ANY', tipo_documento => 'ANY', accion => 'CONSULTA', entorno => 'usuarios'},
 });
 
-
-my $nombre = $input->param('nombre_proveedor');
-my $direccion = $input->param('direccion');
-my $tel = $input->param('telefono');
-my $email = $input->param('email');
+# recibimos solo el id del proveedor y creamos el objeto aca
 my $id_proveedor = $input->param('id_proveedor');
 
-$t_params->{'nombre'} = $nombre;
-$t_params->{'dir'} = $direccion;
-$t_params->{'tel'} = $tel;
-$t_params->{'email'} = $email;
-$t_params->{'id_proveedor'} = $id_proveedor;
+my $proveedor = C4::AR::Proveedores::getProveedorInfoPorId($id_proveedor);
+
+$t_params->{'proveedor'} = $proveedor;
 
 C4::Auth::output_html_with_http_headers($template, $t_params, $session);
