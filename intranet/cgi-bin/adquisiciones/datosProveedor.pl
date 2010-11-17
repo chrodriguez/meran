34621c52b2b197195348d7b2eb8c46645412c81e
Francisco Raposeiras <root@dev.redes>
2010-11-17 09:59:21 -0300

    todo okj esitar proveedor

--- intranet/cgi-bin/adquisiciones/datosProveedor.pl
+++ intranet/cgi-bin/adquisiciones/datosProveedor.pl
@@ -20,6 +20,7 @@ my $id_proveedor = $input->param('id_proveedor');
 
 my $proveedor = C4::AR::Proveedores::getProveedorInfoPorId($id_proveedor);
 
+$t_params->{'id_proveedor'} = $id_proveedor;
 $t_params->{'proveedor'} = $proveedor;
 
 C4::Auth::output_html_with_http_headers($template, $t_params, $session);
