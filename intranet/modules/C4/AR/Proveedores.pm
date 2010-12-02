d9b470d37de2edd0c02c008edb9542af6653434e
Francisco Raposeiras <root@dev.redes>
2010-12-02 12:05:21 -0300

    agregar moneda al %50

--- intranet/modules/C4/AR/Proveedores.pm
+++ intranet/modules/C4/AR/Proveedores.pm
@@ -32,52 +32,37 @@ use vars qw(@EXPORT @ISA);
 =cut
 sub agregarMoneda{
 
-     my ($params) = @_;
+    my ($params) = @_;
 
-    C4::AR::Debug::debug("objeto : ".$params->{'id_proveedor'});
+    my $proveedor_moneda = C4::Modelo::AdqProveedorMoneda->new();
+    my $msg_object= C4::AR::Mensajes::create();
+    my $db = $proveedor_moneda->db;
 
-#      my $id_proveedor = $params->{'id_proveedor'};
-#      my $id_moneda    = $params->{'id_moneda'};
-
-#   hacer objeto esto y llamarlo como tal :
-
-    C4::Modelo::AdqProveedorMoneda::agregarMonedaProveedor($params);
-    return "ok";
-
-    
-
-#     my $proveedor = getProveedorInfoPorId($params->{'id_proveedor'});
-# 
-#     my $msg_object= C4::AR::Mensajes::create();
-#     my $db = $proveedor->db;
-# 
-#     # _verificarDatosProveedor($param,$msg_object);
-# 
-#     if (!($msg_object->{'error'})){
-#           $db->{connect_options}->{AutoCommit} = 0;
-#           $db->begin_work;
-# 
-#           eval{
-#               $proveedor->agregarMoneda($params->{'id_moneda'});
-#               $msg_object->{'error'}= 0;
-#               C4::AR::Mensajes::add($msg_object, {'codMsg'=> 'A023', 'params' => []});
-# #             no agrega nada en 'codMsg'
-#               $db->commit;
-#           };
-# 
-#           if ($@){
-# #           # TODO falta definir el mensaje "amigable" para el usuario informando que no se pudo agregar el proveedor
-#               &C4::AR::Mensajes::printErrorDB($@, 'B449',"INTRA");
-#               $msg_object->{'error'}= 1;
-#               C4::AR::Mensajes::add($msg_object, {'codMsg'=> 'B449', 'params' => []} ) ;
-#               $db->rollback;
-#           }
-# 
-#           $db->{connect_options}->{AutoCommit} = 1;
-#     }
-# 
-#     C4::AR::Debug::debug("msg_object = ".$msg_object->{'codMsg'});
-#     return ($msg_object);
+ 
+     if (!($msg_object->{'error'})){
+           $db->{connect_options}->{AutoCommit} = 0;
+           $db->begin_work;
+ 
+           eval{
+               $proveedor_moneda->agregarMonedaProveedor($params);
+               $msg_object->{'error'}= 0;
+               C4::AR::Mensajes::add($msg_object, {'codMsg'=> 'A023', 'params' => []});
+               $db->commit;
+           };
+ 
+           if ($@){
+                # TODO falta definir el mensaje "amigable" para el usuario informando que no se pudo agregar el proveedor
+               &C4::AR::Mensajes::printErrorDB($@, 'B449',"INTRA");
+               $msg_object->{'error'}= 1;
+               C4::AR::Mensajes::add($msg_object, {'codMsg'=> 'B449', 'params' => []} ) ;
+               $db->rollback;
+           }
+ 
+           $db->{connect_options}->{AutoCommit} = 1;
+     }
+ 
+     C4::AR::Debug::debug("msg_object = ".$msg_object->{'codMsg'});
+     return ($msg_object);
 }
 
 
