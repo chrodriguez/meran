6b929c670796b9e2957db1e0c89e81a81930b921
Gaspar Rajoy <gaspar@gaspar-laptop.(none)>
2011-01-18 16:46:48 -0300

    #790 a Testing

---
+++ intranet/cgi-bin/reports/altas_registro_result.pl
@@ -0,0 +1,70 @@
+#!/usr/bin/perl
+# Copyright 2000-2002 Katipo Communications
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Koha; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+# Suite 330, Boston, MA  02111-1307 USA
+#
+#
+
+use strict;
+use C4::Auth;
+use CGI;
+use C4::AR::Utilidades;
+use C4::AR::Reportes;
+
+my $input = new CGI;
+my $obj=$input->param('obj') || 0;
+
+$obj=C4::AR::Utilidades::from_json_ISO($obj);
+
+my ($template, $session, $t_params, $data_url);
+
+( $template, $session, $t_params ) = get_template_and_user(
+	{
+		#template_name   => "reports/altas_registro_result.tmpl",
+        template_name   => 'busquedas/busquedaResult.tmpl',
+		query           => $input,
+		type            => "intranet",
+		authnotrequired => 0,
+		flagsrequired   => {
+			ui             => 'ANY',
+			tipo_documento => 'ANY',
+			accion         => 'CONSULTA',
+			entorno        => 'undefined'
+		},
+		debug => 1,
+	}
+);
+
+$obj->{'ini'} = $obj->{'ini'} || 1;
+
+my $ini = $obj->{'ini'};
+my ( $ini, $pageNumber, $cantR ) = C4::AR::Utilidades::InitPaginador($ini);
+
+my ( $cantidad, $data ) = C4::AR::Reportes::altasRegistro( $ini, $cantR, $obj );
+
+#my ($path,$filename) = C4::AR::Reportes::toXLS($data,0,'Altas','Altas de Registro');
+#$t_params->{'filename'} = '/reports/'.$filename;
+
+my $funcion = $obj->{'funcion'};
+my $inicial = $obj->{'inicial'};
+
+$t_params->{'paginador'} = C4::AR::Utilidades::crearPaginador( $cantidad, $cantR, $pageNumber,$funcion, $t_params );
+$t_params->{'buscoPor'} = Encode::encode('utf8' ,C4::AR::Busquedas::armarBuscoPor($obj));
+$t_params->{'data'}     = $data;
+$t_params->{'SEARCH_RESULTS'}     = $data;
+$t_params->{'cantidad'} = $cantidad;
+
+C4::Auth::output_html_with_http_headers($template, $t_params, $session);
--- intranet/modules/C4/AR/Busquedas.pm
+++ intranet/modules/C4/AR/Busquedas.pm
@@ -1632,7 +1632,7 @@ sub armarBuscoPor{
         if ($params->{'tipo_nivel3_name'} eq 'ALL'){
         	$buscoPor.= C4::AR::Utilidades::verificarValor(C4::AR::Filtros::i18n("TODOS"))."&";
         }else{
-            $buscoPor.= C4::AR::Utilidades::verificarValor($params->{'tipo_nivel3_name'})."&";
+            $buscoPor.= C4::AR::Utilidades::verificarValor(C4::AR::Referencias::translateTipoNivel3($params->{'tipo_nivel3_name'}))."&";
         	
         }
 	}
