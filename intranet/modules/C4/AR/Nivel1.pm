e662992bc3a1d442c86078ed5141486026796f69
Carbone Miguel <root@KohaDev.linti.unlp.edu.ar>
2011-01-27 13:11:39 -0300

    FIX reindexar

--- intranet/modules/C4/AR/Nivel1.pm
+++ intranet/modules/C4/AR/Nivel1.pm
@@ -82,9 +82,7 @@ sub guardarRealmente{
             $db->commit;            
             #recupero el id1 recien agregado
             $id1 = $catRegistroMarcN1->getId1;
-#             C4::AR::Sphinx::generar_indice($id1);
-# para forzar reindexar
-            C4::AR::Sphinx::generar_indice();
+            C4::AR::Sphinx::generar_indice($id1);
             #ahora el indice se encuentra DESACTUALIZADO
             C4::AR::Preferencias::setVariable('indexado', 0, $db);
             #se cambio el permiso con exito
