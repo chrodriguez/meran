d0d62f97360611d898a0d97cc97c2f4053c6a32f
Magalí Defalco <root@dev.redes>
2011-02-21 11:32:46 -0300

    lsklfja

--- intranet/cgi-bin/adquisiciones/mostrarComparacion.pl
+++ intranet/cgi-bin/adquisiciones/mostrarComparacion.pl
@@ -49,24 +49,21 @@ if($tipoAccion eq "MOSTRAR_PRESUPUESTOS_REC"){
 
 #----------------------------------------------------------------------------
 
-
-          
-          my $renglon;
           my $detalle_renglon;
           my $pres;
           my $cantidad;
           my $precio_unitario;
-
-          my $cant_proveedores = 0; 
           my $proveedor;
+          my @cant_proveedores; 
+
           
           # DETERMINO LA CANTIDAD DE PROVEEDORES
 
 
           foreach my $detalle (@$detalle_rec){
                       if ($proveedor ne ($detalle->{'ref_presupuesto'})->{'proveedor_id'}){
-                            $cant_proveedores= $cant_proveedores + 1;
                             $proveedor= ($detalle->{'ref_presupuesto'})->{'proveedor_id'};
+                            push(@cant_proveedores, $proveedor);
                       }
           }
 
@@ -76,59 +73,62 @@ if($tipoAccion eq "MOSTRAR_PRESUPUESTOS_REC"){
           my @fila_matriz;
           my @fila_renglones;
           my @matriz_comparacion;
-  
-          my $proveedor;
           my $i_fila=0;
-          my $i_col= 1;
+          my $i_col= 0;
           my $detalle;
           my $i=0;
+          my $renglon=1;
           
+
+         
           while ($i != scalar(@$detalle_rec)){
+            
                       $detalle= @$detalle_rec[$i];
-                      $i = $i + 1;
+                     
                       $proveedor= ($detalle->{'ref_presupuesto'})->{'proveedor_id'};
-                      while ($proveedor eq ($detalle->{'ref_presupuesto'})->{'proveedor_id'}) {
-#                              C4::AR::Debug::debug($proveedor);
-                            $renglon= $renglon + 1; 
-                            $i_fila= $i_fila + 1;
-                            @fila_renglones[$i_fila]=$renglon;
-                    
+                     C4::AR::Debug::debug($proveedor);
+                      
+                      while (($proveedor == ($detalle->{'ref_presupuesto'})->{'proveedor_id'}) && ($i != scalar(@$detalle_rec))){
+                            
+                            $i = $i + 1; 
+#                             $renglon= $renglon + 1; 
+#                             $i_fila= $i_fila + 1;
+                        
                             $detalle_renglon=($detalle->{'ref_recomendacion_detalle'})->{'titulo'}." - ".($detalle->{'ref_recomendacion_detalle'})->{'autor'};
-#                           my $nombre_prov=($detalle{'ref_presupuesto'}->{'proveedor_id'})->{'nombre'} || ($detalle{'ref_presupuesto'}->{'proveedor_id'})->{'razon_social'}, 
+#                            my $nombre_prov=($detalle{'ref_presupuesto'}->{'proveedor_id'})->{'nombre'} || ($detalle{'ref_presupuesto'}->{'proveedor_id'})->{'razon_social'}, 
                             $cantidad= $detalle->{'cantidad'};
                             $precio_unitario= $detalle->{'precio_unitario'};
-        
+                            $pres= ($detalle->{'ref_presupuesto'})->{'id'};
+
                             my %hash;
-        #             
+                    
                             %hash= (  renglon => $renglon,
                                       proveedor_id => $proveedor,
         #                             proveedor_nombre => $nombre_prov,
                                       detalle_renglon => $detalle_renglon,
                                       cantidad => $cantidad,
                                       precio => $precio_unitario,
-                                      pres_id => $pres
+                                      pres_id => $pres,
                                     ),
-
+                            
+#                             C4::AR::Utilidades::printHASH(\%hash);
                             @fila_matriz[$i_fila]= \%hash;
+                            $i_fila= $i_fila + 1;
+                            $renglon= $renglon + 1; 
                             $detalle= @$detalle_rec[$i];
-                           
                       }
                         
                       @matriz_comparacion[$i_col]=@fila_matriz;
+                      
                       $renglon=1;
-                      $pres= ($detalle->{'ref_presupuesto'})->{'id'};
                       $i_fila= 0;
-                      $i_col= $i_col + 1;
-                                         
+                      $i_col= $i_col + 1;                         
           }
            
-          @matriz_comparacion[0]=@fila_renglones;
+            
+         $t_params->{'matriz'} = \@matriz_comparacion;
 
-          C4::AR::Utilidades::printARRAY(\@matriz_comparacion);
-        
-          $t_params->{'matriz'} = \@matriz_comparacion;
-   
-#         $t_params->{'proveedores'} = \@proveedores;
+         $t_params->{'proveedores'} = \@cant_proveedores;
        
         C4::AR::Auth::output_html_with_http_headers($template, $t_params, $session);    
 }
