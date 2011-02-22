f6e2b3b2cd5d5c6865cab99f19e3ef972553a320
Francisco Raposeiras <root@dev.redes>
2011-02-22 13:53:08 -0300

    gffs

--- intranet/modules/C4/AR/PedidoCotizacion.pm
+++ intranet/modules/C4/AR/PedidoCotizacion.pm
@@ -4,7 +4,9 @@ use strict;
 require Exporter;
 use DBI;
 use C4::AR::Recomendaciones;
+use C4::AR::PedidoCotizacion;
 use C4::Modelo::AdqPedidoCotizacion;
+use C4::Modelo::AdqPedidoCotizacion::Manager;
 
 use vars qw(@EXPORT @ISA);
 @ISA=qw(Exporter);
@@ -15,13 +17,14 @@ use vars qw(@EXPORT @ISA);
 =item
     Esta funcion agrega un pedido cotizacion
     Parametros: (El id es AUTO_INCREMENT y la fecha CURRENT_TIMESTAMP)
-        HASH { recomendaciones_array, cantidades_array }
+        HASH { id_recomendacion, cantidades_array }
 =cut
 sub addPedidoCotizacion{
 
 # recomendaciones_array, asi obtenemos las recomendaciones y las clonamos en la tabla pedido_cotizacion_detalle
 
     my ($param)             = @_;
+    my $id_recomendacion    = $param->{'id_recomendacion'};
     my $pedido_cotizacion   = C4::Modelo::AdqPedidoCotizacion->new();
     my $msg_object          = C4::AR::Mensajes::create();
     my $db                  = $pedido_cotizacion->db;
@@ -30,31 +33,33 @@ sub addPedidoCotizacion{
         $db->{connect_options}->{AutoCommit} = 0;
         $db->begin_work;
     
-        eval{
-            my %params;        
+      #  eval{
+            my %params;  
+            
+            my %parametros;
+            $parametros{'id_recomendacion'}                     = $param->{'id_recomendacion'};
+            $parametros{'cantidad_ejemplares'}                  = $param->{'cantidad_ejemplares'};                 
         
-            $pedido_cotizacion->addPedidoCotizacion($param);
+            $pedido_cotizacion->addPedidoCotizacion(\%parametros);
 
             my $id_pedido_cotizacion = $pedido_cotizacion->getId();
             
-            # pedido_cotizacion_detalle
-            for(my $i=0;$i<scalar(@{$param->{'recomendaciones_array'}});$i++){
             
-                $params{'id_pedido_recomendacion'}         = $id_pedido_cotizacion;
+            $params{'id_pedido_recomendacion'}         = $id_pedido_cotizacion;
                                
-                # obtenemos una recomendacion_detalle. Para clonar en pedido_cotitazion_detalle
-                my $recomendacion_detalle = C4::AR::Recomendaciones::getRecomendacionDetallePorId($param->{'recomendaciones_array'}->[$i]);
+            # obtenemos una recomendacion_detalle. Para clonar en pedido_cotitazion_detalle
+C4::AR::Debug::debug("id_rec  ".$id_recomendacion);
+            my $recomendacion_detalle = C4::AR::Recomendaciones::getRecomendacionDetallePorId($id_recomendacion);
                             
-                $params{'cantidad_ejemplares'}              = $recomendacion_detalle; 
-                   
-                my $pedido_cotizacion_detalle               = C4::Modelo::AdqPedidoCotizacionDetalle->new(db => $db);    
-                $pedido_cotizacion_detalle->addPedidoCotizacionDetalle(\%params);
-            }                          
+            $params{'cantidad_ejemplares'}              = $recomendacion_detalle; 
+                  
+            my $pedido_cotizacion_detalle               = C4::Modelo::AdqPedidoCotizacionDetalle->new(db => $db);    
+            $pedido_cotizacion_detalle->addPedidoCotizacionDetalle(\%params);                         
             
             $msg_object->{'error'} = 0;
             C4::AR::Mensajes::add($msg_object, {'codMsg'=> 'A033', 'params' => []});
             $db->commit;         
-        };
+       # };
         
         if ($@){
          # TODO falta definir el mensaje "amigable" para el usuario informando que no se pudo agregar el proveedor
