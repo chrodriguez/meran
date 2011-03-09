24265d0f9ae4c4be2b253eb835809292904903ad
Carbone Miguel <root@KohaDev.linti.unlp.edu.ar>
2011-03-09 12:38:47 -0300

    FIX criculacion rapida

--- includes/AjaxHelper.js
+++ includes/AjaxHelper.js
@@ -34,6 +34,8 @@ function _Init(options){
             _ShowState(options);
         }
     }
+    
+     $('#ajax-indicator').ajaxStop(_HiddeState({showStatusIn: options.showStatusIn, showOverlay: options.showOverlay}));
 }
 
 
@@ -66,7 +68,7 @@ function _HiddeState(options){
         $('#state').hide();
     }
 
-    if((options.showOverlay)&&(this.autoClose)){
+    if(options.showOverlay){
         $.modal.close();
     }
 
@@ -182,7 +184,7 @@ function AjaxHelper(fncUpdateInfo, fncInit){
 					url: helper.url,
 					data: params,
  					beforeSend: function(){
-
+                      
 						if(helper.showState){
 						//muestra el estado del AJAX
                             _Init({debug: helper.debug, showStatusIn: helper.showStatusIn, showOverlay: helper.showOverlay});
@@ -195,7 +197,8 @@ function AjaxHelper(fncUpdateInfo, fncInit){
 					},
 					complete: function(ajax){
 						//oculta el estado del AJAX
-						_HiddeState({showStatusIn: helper.showStatusIn, showOverlay: helper.showOverlay});
+//                          $('#ajax-indicator').ajaxStop(_HiddeState({showStatusIn: helper.showStatusIn, showOverlay: helper.showOverlay}));
+// 						_HiddeState({showStatusIn: helper.showStatusIn, showOverlay: helper.showOverlay});
 						if(helper.onComplete){
 							if(ajax.responseText == 'CLIENT_REDIRECT'){
                                     window.location = "/cgi-bin/koha/redirectController.pl";
