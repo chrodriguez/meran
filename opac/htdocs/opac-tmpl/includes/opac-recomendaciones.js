2d8aae1d82f1df45e67b40f0f024c21741e3504f
Magalí Defalco <root@dev.redes>
2011-03-18 12:04:21 -0300

    arch nuevo

---
+++ opac/htdocs/opac-tmpl/includes/opac-recomendaciones.js
@@ -0,0 +1,55 @@
+
+// function agregarDetalleRecomendacion(){
+//     objAH                   = new AjaxHelper(updateAgregarDetalleRecomendacion);
+//     objAH.debug             = true;
+//     objAH.showOverlay       = true;
+//     //para busquedas combinables
+// //     objAH.url               = '/cgi-bin/koha/opac-busquedasDB.pl';
+//     objAH.string            = $('#autor').val();
+//     objAH.string            = $('#titulo').val();
+//     objAH.string            = $('#edicion').val();
+//     objAH.string            = $('#lugar_publicacion').val();
+//     objAH.string            = $('#editorial').val();
+//     objAH.string            = $('#fecha').val();
+//     objAH.string            = $('#coleccion').val();
+//     objAH.string            = $('#ISBN_ISSN').val();
+//     objAH.string            = $('#cant_ejemplares').val();
+//     objAH.tipoAccion        = 'AGREGAR_DETALLE_RECOMENDACION';
+// 
+//     objAH.sendToServer();
+// }
+// 
+// function updateAgregarDetalleRecomendacion(responseText){
+// //    $('#result_busqueda').html(responseText);
+// //    if (!verificarRespuesta(responseText)) {
+// //             return(0);
+// //             var Messages=JSONstring.toObject(responseText);
+// //             setMessages(Messages);
+// //    }
+// }
+ 
+ 
+ function agregarRenglon(){
+   
+//      $('#agregar_rec').click(function(){
+//       if(($('#moneda').val() == "") || ($('#id_moneda').val() == "")){
+//           jConfirm(POR_FAVOR_INGRESE_UNA_MONEDA, function(){ })      
+//       }else{
+          var autor  = $('#autor').val()
+          var titulo   = $('#titulo').val()
+          var edicion = $('#edicion').val()
+          var lugar_publicacion = $('#lugar_publicacion').val();
+          var editorial= $('#editorial').val();
+          var fecha = $('#fecha').val();
+          var coleccion = $('#coleccion').val();
+          var ISBN_ISSN = $('#isbn_issn').val();
+          var cant_ejemplares = $('#cant_ejemplares').val();
+          
+          $('#tabla_recomendacion').append(
+              '<tr><td>'+autor+'</td><td>'+titulo+'</td><td>'+edicion+'</td><td>'+lugar_publicacion+'</td>'+
+              '<td>'+editorial+'</td><td>'+fecha+'</td><td>'+autor+'</td><td>'+coleccion+'</td><td>'+ISBN_ISSN+'</td><td>'+cant_ejemplares+'</td></tr>'
+           ) 
+      }
+          
+/* } */    
+ 
