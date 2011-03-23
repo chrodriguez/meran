93e0cca15e6432b0fd1479c96aa7b567f8cf03f2
Francisco Raposeiras <root@dev.redes>
2011-03-23 12:45:13 -0300

    fixes de proveedores

--- intranet/cgi-bin/adquisiciones/addProveedores.pl
+++ intranet/cgi-bin/adquisiciones/addProveedores.pl
@@ -24,52 +24,7 @@ my ($template, $session, $t_params) = get_template_and_user({
 if($obj){
     $obj= C4::AR::Utilidades::from_json_ISO($obj);
     
-    my %params = {};
-
-    $params{'tipo_proveedor'}       = $obj->{'tipo_proveedor'};
-
-#   dependiendo del tipo se guarda ciertos campos
-    if($params{'tipo_proveedor'} eq "persona_fisica"){
-
-        $params{'apellido'}         = $obj->{'apellido'};
-        $params{'nombre'}           = $obj->{'nombre'};   
-        $params{'tipo_doc'}         = $obj->{'tipo_doc'};
-        $params{'nro_doc'}          = $obj->{'nro_doc'};  
-      
-    }else{
-
-        $params{'razon_social'}     = $obj->{'razon_social'}; 
-    }
-
-    $params{'cuit_cuil'}            = $obj->{'cuit_cuil'};     
-    $params{'pais'}                 = $obj->{'pais'};
-    $params{'provincia'}            = $obj->{'provincia'};
-    $params{'ciudad'}               = $obj->{'ciudad'};   
-    $params{'domicilio'}            = $obj->{'domicilio'};
-    $params{'telefono'}             = $obj->{'telefono'};
-    $params{'fax'}                  = $obj->{'fax'};
-    $params{'email'}                = $obj->{'email'};
-    $params{'plazo_reclamo'}        = $obj->{'plazo_reclamo'};
-
-    $params{'proveedor_activo'}     = 1; 
-    $params{'tipoAccion'}           = $obj->{'tipoAccion'};
-
-# Monedas:
-
-    $params{'monedas_array'}        = $obj->{'monedas_array'}; 
-
-# Tipo de materiales:
-
-    $params{'materiales_array'}     = $obj->{'materiales_array'}; 
-    
-# Formas de envio:
-
-    $params{'formas_envios_array'}     = $obj->{'formas_envios_array'}; 
-
-
-# FIXME pueden pasar directamente $obj a agregarProveedor es una HASH = a $params
-
-    my ($message) = C4::AR::Proveedores::agregarProveedor(\%params);
+    my ($message) = C4::AR::Proveedores::agregarProveedor($obj);
     my $infoOperacionJSON=to_json $message;
 
     C4::AR::Auth::print_header($session);
@@ -78,7 +33,7 @@ if($obj){
 }else{
 # mostramos el template porque esta agregando normalmente
 
-     my $comboDeTipoDeDoc       = &C4::AR::Utilidades::generarComboTipoDeDoc();
+     my $comboDeTipoDeDoc       = &C4::AR::Utilidades::generarComboTipoDeDocConValuesIds();
      my $combo_tipo_materiales  = &C4::AR::Utilidades::generarComboTipoDeMaterial();
      my $combo_formas_envio     = &C4::AR::Utilidades::generarComboFormasDeEnvio();
 
--- intranet/cgi-bin/adquisiciones/datosProveedor.pl
+++ intranet/cgi-bin/adquisiciones/datosProveedor.pl
@@ -20,7 +20,7 @@ if ($tipoAccion eq "EDITAR") {
             query       => $input,
             type        => "intranet",
             authnotrequired => 0,
-            flagsrequired   => { ui => 'ANY', tipo_documento => 'ANY', accion => 'MODIFICAR', entorno => 'adquisiciones'},
+            flagsrequired   => { ui => 'ANY', tipo_documento => 'ANY', accion => 'CONSULTA', entorno => 'usuarios'},
     });
     
     my $comboDeTipoDeDoc = &C4::AR::Utilidades::generarComboTipoDeDoc();
@@ -34,24 +34,17 @@ if ($tipoAccion eq "EDITAR") {
                       query       => $input,
                       type        => "intranet",
                       authnotrequired => 0,
-                      flagsrequired   => { ui => 'ANY', tipo_documento => 'ANY', accion => 'CONSULTA', entorno => 'adquisiciones'},
+                      flagsrequired   => { ui => 'ANY', tipo_documento => 'ANY', accion => 'CONSULTA', entorno => 'usuarios'},
           });    
 }
 
-my $monedas                             = C4::AR::Proveedores::getMonedasProveedor($id_proveedor);
-my $formas_envio                        = C4::AR::Proveedores::getFormasEnvioProveedor($id_proveedor);
-my $proveedor                           = C4::AR::Proveedores::getProveedorInfoPorId($id_proveedor);
-my $tipo_materiales                     = &C4::AR::Utilidades::generarComboTipoDeMaterial();
-my $materiales_proveedor                = C4::AR::Proveedores::getMaterialesProveedor($id_proveedor);
-my $formas_envio                        = &C4::AR::Utilidades::generarComboFormasDeEnvio();
-my $formas_envio_proveedor              = C4::AR::Proveedores::getFormasEnvioProveedor($id_proveedor);
-
-$t_params->{'formas_envio'}             = $formas_envio;
-$t_params->{'proveedor'}                = $proveedor;
-$t_params->{'monedas'}                  = $monedas;
-$t_params->{'tipo_materiales'}          = $tipo_materiales; 
-$t_params->{'materiales_proveedor'}     = $materiales_proveedor;
-$t_params->{'formas_envio'}             = $formas_envio;
-$t_params->{'formas_envio_proveedor'}   = $formas_envio_proveedor;
+$t_params->{'formas_envio'}             = C4::AR::Proveedores::getFormasEnvioProveedor($id_proveedor);
+$t_params->{'proveedor'}                = C4::AR::Proveedores::getProveedorInfoPorId($id_proveedor);
+$t_params->{'monedas'}                  = C4::AR::Proveedores::getMonedasProveedor($id_proveedor);
+$t_params->{'tipo_materiales'}          = &C4::AR::Utilidades::generarComboTipoDeMaterial();
+$t_params->{'materiales_proveedor'}     = C4::AR::Proveedores::getMaterialesProveedor($id_proveedor);
+$t_params->{'formas_envio'}             = &C4::AR::Utilidades::generarComboFormasDeEnvio();
+$t_params->{'formas_envio_proveedor'}   = C4::AR::Proveedores::getFormasEnvioProveedor($id_proveedor);
+$t_params->{'persona_fisica'}           = C4::AR::Proveedores::isPersonaFisica($id_proveedor);
 
 C4::AR::Auth::output_html_with_http_headers($template, $t_params, $session);
