ef878ddafee3e564d9a23ea1f838ce92b481d4f4
Gaspar Rajoy <gaspar@gaspar-laptop.(none)>
2011-03-23 12:17:44 -0300

    fixed #1763

--- intranet/modules/C4/AR/Usuarios.pm
+++ intranet/modules/C4/AR/Usuarios.pm
@@ -667,7 +667,7 @@ C4::AR::Debug::debug("Usuarios => getSocioLike => orden => ".$orden);
             foreach my $s (@searchstring_array){ 
                 push (	@filtros, ( or   => [   
                                                 'persona.nombre'    => { like => '%'.$s.'%'},   
-                                                apellido            => { like => '%'.$s.'%'},
+                                                apellido            => { like => $s.'%'},
                                                 nro_documento       => { like => '%'.$s.'%' }, 
                                                 legajo              => { like => '%'.$s.'%' },
                                                 nro_socio           => { like => '%'.$s.'%' }          
@@ -687,6 +687,7 @@ C4::AR::Debug::debug("Usuarios => getSocioLike => orden => ".$orden);
 
     push(@filtros, ( activo => { eq => $habilitados}));
     push(@filtros, ( es_socio => { eq => $habilitados}));
+    $orden = "apellido,nombre";
     my $ordenAux= $socioTemp->sortByString($orden);
 
     my $socios_array_ref = C4::Modelo::UsrSocio::Manager->get_usr_socio(   query => \@filtros,
