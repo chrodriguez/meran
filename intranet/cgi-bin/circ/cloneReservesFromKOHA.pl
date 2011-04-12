64cf6fa6d7936e5a08d1df154abd4e01356d1303
Gaspar Rajoy <gaspar@gaspar-laptop.(none)>
2011-04-12 14:51:11 -0300

    #1858

--- intranet/cgi-bin/circ/cloneReservesFromKOHA.pl
+++ intranet/cgi-bin/circ/cloneReservesFromKOHA.pl
@@ -9,4 +9,33 @@ use C4::Context;
 my $input = new CGI;
 
 #Loggear todo!!!!!!!!!!!!!!
+my $reserva = $input->Vars;
 
+my $id1 = $reserva->{'id1'};
+my $id2 = $reserva->{'id2'};
+my $nro_socio = $reserva->{'nro_socio'};
+my $fecha_reserva = $reserva->{'fecha'};
+my $nro_reserva = $reserva->{'nro_reserva'};
+
+
+my %params;
+
+$params{'tipo'}= 'OPAC';
+$params{'id1'}= $id1;
+$params{'id2'}= $id2;
+$params{'nro_socio'}= $nro_socio;
+$params{'loggedinuser'}= $nro_socio;
+$params{'tipo_prestamo'}= 'DO';
+
+my ($msg_object)= C4::AR::Reservas::t_reservarOPAC(\%params);
+
+if ($msg_object->{'error'}){
+    C4::AR::Debug::debug("************** ERROR EN CLONAR RESERVA DESDE KOHA *******************");
+    C4::AR::Debug::debug("    ************************************************************     ");
+    C4::AR::Debug::debug("           ************************************************          ");
+    C4::AR::Debug::debug("             Checkear la reserva numero ".$nro_reserva."  NO SE PUDO RESERVAR!!!");
+    C4::AR::Debug::debug("             Sobre el grupo ".$id2."  DEL SOCIO ".$nro_socio);
+    C4::AR::Debug::debug("           ************************************************          ");
+    C4::AR::Debug::debug("    ************************************************************     ");
+    C4::AR::Debug::debug("*********************************************************************");
+}
