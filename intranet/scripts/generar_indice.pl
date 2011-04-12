8c23a3954268389b0ef95ba112c61c2f86e46b7d
Matías Pagano <matiasp@mail.linti.unlp.edu.ar>
2011-04-12 11:35:43 -0300

    generar indice

--- intranet/scripts/generar_indice.pl
+++ intranet/scripts/generar_indice.pl
@@ -2,7 +2,7 @@
 use C4::AR::Sphinx;
 
 my $id1 = $ARGV[0] || '0'; #id1 del registro
-
-    C4::AR::Sphinx::generar_indice($id1);
+my $flag = $ARGV[1] || 'R_FULL'; #id1 del registro
+    C4::AR::Sphinx::generar_indice($id1,$flag);
 
 1;
