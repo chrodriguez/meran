6f7476963ea970f4dee40dfac23e5ba78736ca95
Matías Pagano <matiasp@mail.linti.unlp.edu.ar>
2011-05-10 14:02:45 -0300

    meran

---
+++ docs/dependenciasInstalacion/dependenciasMERAN.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+apt-get update -y
+apt-get install apache2 mysql-server libpdf-report-perl libhtml-template-expr-perl  libhtml-template-perl libnet-ldap-perl libdbd-mysql-perl libmail-sendmail-perl libmarc-record-perl libgd-gd2-perl libarchive-zip-perl libapache2-mod-perl2 libapache-db-perl libooolib-perl libpdf-report-perl  libchart-perl libtemplate-perl libnet-amazon-perl libnet-z3950-zoom-perl  aspell-es libtext-aspell-perl libnet-smtp-ssl-perl libspreadsheet-writeexcel-perl htmldoc libyaml-dev -y 
+
+# MUCHOS ESTAN EN SQUEEZE!!!
+apt-get install libjson-perl libjson-xs-perl librose-db-perl librose-db-object-perl sphinxsearch libsphinx-search-perl libdigest-sha-perl libcrypt-cbc-perl libtext-levenshtein-perl  libnet-smtp-tls-perl libnet-ssleay-perl libnet-twitter-perl libwww-facebook-api-perl libspreadsheet-read-perl -y
+
+apt-get install libcgi-session-perl libdate-manip-perl liblocale-maketext-gettext-perl libspreadsheet-writeexcel-perl -y
+
+# OTROS AUN NO!!!
+apt-get install make gcc -y
+cpan -i CPAN 
+cpan -i YAML
+
+cpan -i Spreadsheet::WriteExcel::Simple
+cpan -i Sphinx::Manager
+cpan -i Image::Resize
+cpan -i Text::LevenshteinXS
+cpan -i Chart::OFC2
+cpan -i WWW::Shorten::Bitly
+cpan -i HTML::HTMLDoc
+cpan -i MARC::File::XML
+cpan -i DateTime::Format::DateManip
