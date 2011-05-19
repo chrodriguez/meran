42ec9bc73bb6d547d5945a62da8bc5f6fa709aa6
Matías Pagano <matiasp@mail.linti.unlp.edu.ar>
2011-05-19 15:54:12 -0300

    test mail disponibilidad

--- intranet/modules/C4/AR/Reservas.pm
+++ intranet/modules/C4/AR/Reservas.pm
@@ -12,6 +12,7 @@ use C4::Modelo::CircReserva;
 use C4::Modelo::CircReserva::Manager;
 use C4::Modelo::CatRegistroMarcN3::Manager;
 
+
 use vars qw($VERSION @ISA @EXPORT %EXPORT_TAGS);
 
 $VERSION = 3.0;
@@ -529,7 +530,7 @@ sub Enviar_Email_Cancelacion_Reserva{
         $mail{'mail_from'}             = $mailFrom;
         $mail{'mail_to'}               = $socio->persona->getEmail;
         $mail{'mail_subject'}          = $mailSubject;
-        $mail{'mail_message'}          = $mailMessage;
+	$mail{'mail_message'}          = $mailMessage;
 
        C4::AR::Debug::debug("Reservas => Enviar_Email_Cancelacion_Reserva => Se envia el mail ".$mail{'mail_message'});
 
