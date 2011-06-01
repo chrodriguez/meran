ef7bce27a672f01d7715022aba99a8180866b769
Magalí Defalco <root@dev.redes>
2011-06-01 10:47:31 -0300

    prueba

--- intranet/modules/C4/AR/Auth.pm
+++ intranet/modules/C4/AR/Auth.pm
@@ -700,10 +700,11 @@ sub checkauth {
                                             $mensaje= 'U357';
              
                              }
+                              C4::AR::Debug::debug("QUERY    ".$query->url_param('welcome'));
                               if (!$query->url_param('welcome')){
                                       $template_params->{'loginAttempt'} = 1;  
                               }
-                                      _destruirSession($mensaje, $template_params);
+                              _destruirSession($mensaje, $template_params);
 
 
                   }# end unless ($userid)
