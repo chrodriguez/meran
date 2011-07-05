22b8de839125eebf4c4ceeb22566a60cdfdc6910
Francisco Raposeiras <root@dev.redes>
2011-07-05 12:30:04 -0300

    historial reservas

--- intranet/cgi-bin/usuarios/reales/historialPrestamos.pl
+++ intranet/cgi-bin/usuarios/reales/historialPrestamos.pl
@@ -3,44 +3,31 @@
 use strict;
 use C4::AR::Auth;
 use C4::Date;
-
 use CGI;
 
 my $input=new CGI;
-
 my ($template, $session, $t_params) =  get_template_and_user ({
 							template_name	=> 'usuarios/reales/historialPrestamos.tmpl',
-							query		=> $input,
-							type		=> "intranet",
+							query		    => $input,
+							type		    => "intranet",
 							authnotrequired	=> 0,
-							flagsrequired	=> {    ui => 'ANY', 
-                                                    tipo_documento => 'ANY', 
-                                                    accion => 'CONSULTA', 
-                                                    entorno => 'undefined'},
+							flagsrequired	=> {    ui              => 'ANY', 
+                                                    tipo_documento  => 'ANY', 
+                                                    accion          => 'CONSULTA', 
+                                                    entorno         => 'undefined'},
     });
 
-
-my $obj=C4::AR::Utilidades::from_json_ISO($input->param('obj'));
-
+my $obj = C4::AR::Utilidades::from_json_ISO($input->param('obj'));
 C4::AR::Validator::validateParams('U389',$obj,['nro_socio'] );
-
-my $nro_socio= $obj->{'nro_socio'};
-my $orden= $obj->{'orden'}||'date_due desc';
-my $ini= $obj->{'ini'};
-my $funcion= $obj->{'funcion'};
-
-
-my ($ini,$pageNumber,$cantR)=C4::AR::Utilidades::InitPaginador($ini);
-
-# my ($cant,$presmamos_array_ref,$loop_reading)=C4::AR::Prestamos::getHistorialPrestamosParaTemplate($nro_socio,$ini,$cantR,$orden);
-
-my ($cant,$prestamos_array_ref)=C4::AR::Prestamos::getHistorialPrestamosParaTemplate($nro_socio,$ini,$cantR, $orden);
-
-$t_params->{'historial'}= $prestamos_array_ref;
-$t_params->{'paginador'}=&C4::AR::Utilidades::crearPaginador($cant,$cantR, $pageNumber,$funcion,$t_params);
-$t_params->{'cant'}= $cant;
-$t_params->{'nro_socio'}= $nro_socio;
-# $t_params->{'loop_reading'}= $loop_reading;
+my $nro_socio                   = $obj->{'nro_socio'};
+my $orden                       = $obj->{'orden'}||'date_due desc';
+my $ini                         = $obj->{'ini'};
+my $funcion                     = $obj->{'funcion'};
+my ($ini,$pageNumber,$cantR)    = C4::AR::Utilidades::InitPaginador($ini);
+my ($cant,$prestamos_array_ref) = C4::AR::Prestamos::getHistorialPrestamosParaTemplate($nro_socio,$ini,$cantR, $orden);
+$t_params->{'historial'}        = $prestamos_array_ref;
+$t_params->{'paginador'}        =&C4::AR::Utilidades::crearPaginador($cant,$cantR, $pageNumber,$funcion,$t_params);
+$t_params->{'cant'}             = $cant;
+$t_params->{'nro_socio'}        = $nro_socio;
 
 C4::AR::Auth::output_html_with_http_headers($template, $t_params, $session);
-
