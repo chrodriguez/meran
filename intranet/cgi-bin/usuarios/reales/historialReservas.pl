60399ff79c58a5a0cefa8dee8e734d41971e2b05
Francisco Raposeiras <root@dev.redes>
2011-07-07 16:49:30 -0300

    varios fran

--- intranet/cgi-bin/usuarios/reales/historialReservas.pl
+++ intranet/cgi-bin/usuarios/reales/historialReservas.pl
@@ -24,12 +24,13 @@ my $obj = C4::AR::Utilidades::from_json_ISO($input->param('obj'));
 C4::AR::Validator::validateParams('U389',$obj,['nro_socio'] );
 
 my $nro_socio                   = $obj->{'nro_socio'};
-my $orden                       = $obj->{'orden'}||'date_due desc';
+my $orden                       = $obj->{'orden'}||'id DESC';
 my $ini                         = $obj->{'ini'};
 my $funcion                     = $obj->{'funcion'};
+
 my ($ini,$pageNumber,$cantR)    = C4::AR::Utilidades::InitPaginador($ini);
 my ($cant,$reservas_array_ref)  = C4::AR::Reservas::getHistorialReservasParaTemplate($nro_socio,$ini,$cantR,$orden);
-#FIXME: arreglar paginador
+
 $t_params->{'paginador'}        = C4::AR::Utilidades::crearPaginador($cant,$cantR, $pageNumber,$funcion,$t_params);
 $t_params->{'cantidad'}         = $cant;
 $t_params->{'nro_socio'}        = $nro_socio;
