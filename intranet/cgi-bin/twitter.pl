25cf2d4977e3f575e98c331fafe189ad85155d8c
Gaspar Rajoy <gaspar@desarrollo-meran.(none)>
2011-07-13 10:43:05 -0300

    varios twitter

--- intranet/cgi-bin/twitter.pl
+++ intranet/cgi-bin/twitter.pl
@@ -7,6 +7,10 @@ use CGI;
 my $user = 'koha_unlp';
 my $password = 'pato123@'; 
 
+my $consumer_key        = "Bk6HpbcEPLWWKJdYHjTJXQ";
+my $consumer_secret     = "IRo34n7Mkd2VRhinUGXoInKsoxVub9ubanBSmmuLIg";
+my $token               = "148446079-6piw6kAePtptOnAQK3hIxWxKXXhPhW95u4gxRuBE";
+my $token_secret        = "rPyA0Xkdl05ehFOEXkVA0ENlAGsnyZAiN2WKZb1zOw"; 
 
 my $url = "http://www.google.com";
 
@@ -16,12 +20,13 @@ my $short_url = makeashorterlink($url, 'gaspo53', 'R_2123296565094a87c392b184d2a
 print "\n Short Url: ".$short_url."\n";
 
 my $nt = Net::Twitter->new(
-    traits   => [qw/API::REST/],
-    username => $user,
-    password => $password,
-    clientname => "Twitter for MERAN",
-    source => '',
+    traits   => [qw/OAuth API::REST/],
+    consumer_key        => $consumer_key,
+    consumer_secret     => $consumer_secret,
+    access_token        => $token,
+    access_token_secret => $token_secret,
 );
+
 
 my $result = $nt->update($ARGV[0]);
 
