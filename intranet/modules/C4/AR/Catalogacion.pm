fe4675bdacd87c9869e6f8d3aadba2231d61b982
Carbone Miguel <root@dev.redes>
2011-09-02 12:06:42 -0300

    FIX overflow truncate

--- intranet/modules/C4/AR/Catalogacion.pm
+++ intranet/modules/C4/AR/Catalogacion.pm
@@ -503,7 +503,7 @@ sub marc_record_to_meran_to_detail_view {
                 #C4::AR::Debug::debug("Catalogacion => marc_record_to_meran_to_detail_view => orden: ".$hash_temp{'orden'});
                 $dato                               = getRefFromStringConArrobasByCampoSubcampo($campo, $subcampo, $dato, $itemtype, $db);
                 $hash_temp{'datoReferencia'}        = $dato;
-                my $valor_referencia                = getDatoFromReferencia($campo, $subcampo, $dato, $itemtype,$db);
+                my $valor_referencia                = getDatoFromReferencia($campo, $subcampo, $dato, $itemtype, $db);
 
                 $hash_temp{'dato'}                  = C4::AR::Filtros::show_componente(   
                                                                         campo       => $hash_temp{'campo'},
