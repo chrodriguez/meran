8ba5cdb17f851d1b00cd199fb41550755d74f926
Francisco Raposeiras <root@dev.redes>
2011-09-05 11:31:02 -0300

    fix en opacbusqueda con acentos

--- opac/cgi-bin/opac-busquedasDB.pl
+++ opac/cgi-bin/opac-busquedasDB.pl
@@ -5,77 +5,69 @@ use CGI;
 use C4::AR::Auth;
 use C4::Output;
 use JSON;
-
 use C4::AR::Busquedas;
 use Time::HiRes;
+use Encode;
+use URI::Escape;
 
-my $input = new CGI;
-
-my $obj=$input->param('obj');
-
-
+my $input                   = new CGI;
+my $obj                     = $input->param('obj');
+my %hash_temp               = $input->Vars;
+$obj                        = \%hash_temp;
+#Se usa $params_hash para guardar los originales y hacer la URL del paginador, sino pasa que se hace encode de encode y rompe
+my $params_hash             = $input->Vars;
+my $string                  = $input->param('string') || "";
 my ($template, $session, $t_params);
 
-my %hash_temp = $input->Vars;
-$obj = \%hash_temp;
-
-#Se usa $params_hash para guardar los originales y hacer la URL del paginador, sino pasa que se hace encode de encode y rompe
-my $params_hash =   $input->Vars;
-my $string = $input->param('string') || "";
-
-$obj->{'tipoAccion'} = $input->param('tipoAccion');
-$obj->{'string'} = Encode::decode_utf8($string);
-$obj->{'titulo'} = Encode::decode_utf8($input->param('titulo'));
-$obj->{'autor'} = Encode::decode_utf8($input->param('autor'));
-$obj->{'isbn'} = Encode::decode_utf8($input->param('isbn'));
-$obj->{'estantes'} = Encode::decode_utf8($input->param('estantes'));
-$obj->{'estantes_grupo'} = Encode::decode_utf8($input->param('estantes_grupo'));
-$obj->{'tema'} = Encode::decode_utf8($input->param('tema'));
-$obj->{'tipo'} = $input->param('tipo');    
-$obj->{'only_available'} = $input->param('only_available') || 0;
-$obj->{'from_suggested'} = $input->param('from_suggested');
-$obj->{'tipo_nivel3_name'} = $input->param('tipo_nivel3_name');
-$obj->{'tipoBusqueda'} = 'all';
-$obj->{'token'} = $input->param('token');
-my $ini = $obj->{'ini'} = $input->param('page') || 0;
-
-
-my $start = [ Time::HiRes::gettimeofday() ]; #se toma el tiempo de inicio de la búsqueda
+$obj->{'tipoAccion'}        = $input->param('tipoAccion');
+#solucion a la busqueda con acentos, se hace uri_escape para pasar el parametro por la url
+#en Busquedas.pm se decodea utf8 y sacan acentos para sphinx
+$obj->{'string'}            = uri_escape($string);
+$obj->{'titulo'}            = Encode::decode_utf8($input->param('titulo'));
+$obj->{'autor'}             = Encode::decode_utf8($input->param('autor'));
+$obj->{'isbn'}              = Encode::decode_utf8($input->param('isbn'));
+$obj->{'estantes'}          = Encode::decode_utf8($input->param('estantes'));
+$obj->{'estantes_grupo'}    = Encode::decode_utf8($input->param('estantes_grupo'));
+$obj->{'tema'}              = Encode::decode_utf8($input->param('tema'));
+$obj->{'tipo'}              = $input->param('tipo');    
+$obj->{'only_available'}    = $input->param('only_available') || 0;
+$obj->{'from_suggested'}    = $input->param('from_suggested');
+$obj->{'tipo_nivel3_name'}  = $input->param('tipo_nivel3_name');
+$obj->{'tipoBusqueda'}      = 'all';
+$obj->{'token'}             = $input->param('token');
+my $ini                     = $obj->{'ini'} = $input->param('page') || 0;
+my $start                   = [ Time::HiRes::gettimeofday() ]; #se toma el tiempo de inicio de la búsqueda
 
 my $cantidad;
 my $suggested;
 my $resultsarray;
 
-$obj->{'type'} = 'OPAC';
-$obj->{'session'}= $session;
-
+$obj->{'type'}              = 'OPAC';
+$obj->{'session'}           = $session;
 
 # PAGINADOR
-my ($ini,$pageNumber,$cantR)=C4::AR::Utilidades::InitPaginador($ini);
+my ($ini,$pageNumber,$cantR) = C4::AR::Utilidades::InitPaginador($ini);
 #actualizamos el ini del $obj para que pagine correctamente
-$obj->{'ini'} = $ini;
-$obj->{'cantR'} = $cantR;
-
-C4::AR::Debug::debug(" CANTIDAD REGISTROS A MOSTRAR EN BUSQUEDAAAAAAAAAAAAAAAAAAAAAAAA ".$cantR);
+$obj->{'ini'}               = $ini;
+$obj->{'cantR'}             = $cantR;
 
 C4::AR::Validator::validateParams('U389',$obj,['tipoAccion']);
 
-$obj->{'from_suggested'}= $obj->{'from_suggested'};
-
+$obj->{'from_suggested'}    = $obj->{'from_suggested'};
 
 my $url;
 my $url_todos;
 my $token;
 
-    ($template, $session, $t_params)= get_template_and_user({
-                        template_name => "opac-main.tmpl",
-                        query => $input,
-                        type => "opac",
+    ($template, $session, $t_params)    = get_template_and_user({
+                        template_name   => "opac-main.tmpl",
+                        query           => $input,
+                        type            => "opac",
                         authnotrequired => 1,
-                        flagsrequired => {  ui => 'ANY', 
-                                            tipo_documento => 'ANY', 
-                                            accion => 'CONSULTA', 
-                                            entorno => 'undefined'},
+                        flagsrequired   => {  ui            => 'ANY', 
+                                            tipo_documento  => 'ANY', 
+                                            accion          => 'CONSULTA', 
+                                            entorno         => 'undefined'},
                     });
 
 
@@ -118,18 +110,14 @@ if($obj->{'estantes_grupo'}){
     C4::AR::Utilidades::addParamToUrl($url_todos,"tema",$obj->{'tema'});
     C4::AR::Utilidades::addParamToUrl($url_todos,"autor",$obj->{'autor'});
     
-
     ($cantidad, $resultsarray)= C4::AR::Busquedas::busquedaAvanzada_newTemp($obj,$session);
   }
 }
 }   else {
-
     $url = C4::AR::Utilidades::getUrlPrefix()."/opac-busquedasDB.pl?token=".$obj->{'token'}."&string=".$obj->{'string'}."&tipoAccion=".$obj->{'tipoAccion'}."&only_available=".$obj->{'only_available'};
     $url_todos = C4::AR::Utilidades::getUrlPrefix()."/opac-busquedasDB.pl?token=".$obj->{'token'}."&string=".$obj->{'string'}."&tipoAccion=".$obj->{'tipoAccion'};
     
     ($cantidad, $resultsarray,$suggested)  = C4::AR::Busquedas::busquedaCombinada_newTemp($string,$session,$obj);
-
-
 } 
 
 if ($obj->{'estantes'}||$obj->{'estantes_grupo'}){
@@ -144,7 +132,10 @@ $t_params->{'suggested'}                = $suggested;
 $t_params->{'tipoAccion'}               = $obj->{'tipoAccion'};
 $t_params->{'url_todos'}                = $url_todos;
 $t_params->{'only_available'}           = $obj->{'only_available'};
+C4::AR::Debug::debug("url antes de entrar a paginadoropac : ".$url);
 $t_params->{'paginador'}                = C4::AR::Utilidades::crearPaginadorOPAC($cantidad,$cantR, $pageNumber,$url,$t_params);
+C4::AR::Debug::debug("url despues de entrar a paginadoropac : ".$url);
+C4::AR::Debug::debug("paginador : ".$t_params->{'paginador'});
 $t_params->{'combo_tipo_documento'}     = C4::AR::Utilidades::generarComboTipoNivel3();
 
 #se arma el arreglo con la info para mostrar en el template
@@ -156,14 +147,13 @@ $obj->{'nro_socio'}                     = $session->param('nro_socio');
 $t_params->{'SEARCH_RESULTS'}           = $resultsarray;
 
 #se arma el string para mostrar en el cliente lo que a buscado, ademas escapa para evitar XSS
-# $obj->{'keyword'} = Encode::decode_utf8($obj->{'string'});
+#primero se unescapea por la url, arriba se escapo
+$obj->{'keyword'}                       = uri_unescape($obj->{'string'});
+$t_params->{'keyword'}                  = $obj->{'keyword'};
+$t_params->{'buscoPor'}                 = C4::AR::Busquedas::armarBuscoPor($obj);
 
-$obj->{'keyword'}               = $obj->{'string'};
-$t_params->{'keyword'}          = $obj->{'keyword'};
-$t_params->{'buscoPor'}         = Encode::encode_utf8(C4::AR::Busquedas::armarBuscoPor($obj));
-
-$t_params->{'cantidad'}         = $cantidad || 0;
-$t_params->{'show_search_details'} = 1;
+$t_params->{'cantidad'}                 = $cantidad || 0;
+$t_params->{'show_search_details'}      = 1;
 
 
 C4::AR::Auth::output_html_with_http_headers($template, $t_params, $session);
