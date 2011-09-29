5b3835e42e39a62397ab516a688a62524d24f1e1
Gaspar Rajoy <gaspar@desarrollo-meran.(none)>
2011-09-29 14:52:37 -0300

    NO CHANGELOG - fix en Date.pm

--- intranet/modules/C4/Date.pm
+++ intranet/modules/C4/Date.pm
@@ -292,12 +292,12 @@ sub proximosHabiles {
 	}else{
         #esto es si no importa quetodos los dias del periodo sean habiles, los que deben ser habiles son el 1ero y el ultimo		
 		$hasta = DateCalc($desde,"+ ".$cantidad. " days",\$err,2);
+	    if (!esHabil($hasta)){
+	        $hasta = Date_NextWorkDay($hasta);
+	    } 
 	}
 
         C4::AR::Debug::debug("********** HASTA DESPUES DEL CALCULO `primero ".$hasta);
-    if (!esHabil($hasta)){
-        $hasta = Date_NextWorkDay($hasta);
-    } 
         C4::AR::Debug::debug("********** HASTA DESPUES DEL CALCULO segundo ".$hasta);
 
 	#Se sume un dia si es feriado el ultimo dia.
