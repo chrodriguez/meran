bb42677b0548dc664f364ef20dcf6908caf14a55
Carbone Miguel <root@dev.redes>
2011-10-28 18:03:20 -0300

    configuracion de tablas de referencia

--- intranet/modules/C4/Modelo/PrefTablaReferenciaConf.pm
+++ intranet/modules/C4/Modelo/PrefTablaReferenciaConf.pm
@@ -20,7 +20,38 @@ __PACKAGE__->meta->setup(
 
 use C4::Modelo::PrefTablaReferenciaConf::Manager;
 
+sub getConf{
+    my ($self) = shift;
+    my ($tabla, $campo) = @_;
 
+
+    my @filtros;
+
+    push (@filtros, (campo => {eq => $campo}) );
+    push (@filtros, (tabla => {eq => $tabla}) );
+
+    my $configuarcion = C4::Modelo::PrefTablaReferenciaConf::Manager->get_pref_tabla_referencia_conf(  query => \@filtros,
+#                                                                                     select    => ['id1'],
+                                                                                  );
+
+    if(scalar(@$configuarcion) > 0){
+        return $configuarcion->[0];
+    } else {
+        return 0;
+    }
+}
+
+sub getVisible{
+    my ($self) = shift;
+
+    return ($self->visible);
+}
+
+sub getCampoAlias{
+    my ($self) = shift;
+
+    return ($self->campo_alias);
+}
 
 1;
 
