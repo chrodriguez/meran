71cf2cbec8a2ccc09fa887da11cc6b84150b6de5
Gaspar Rajoy <gaspar@desarrollo-meran.(none)>
2011-11-02 12:11:55 -0300

    [BUGFIX] en el encoding de postear una novedad de OPAC hacia Twitter

--- intranet/cgi-bin/cron/reindexar.pl
+++ intranet/cgi-bin/cron/reindexar.pl
@@ -5,11 +5,21 @@ require Exporter;
 use C4::Context;
 use CGI;
 use C4::AR::Sphinx qw(reindexar);
+use Fcntl qw(:flock);
 
-C4::AR::Debug::debug("CRON => reindexar.pl!!!!!");
+unless (!flock(DATA, LOCK_EX|LOCK_NB)) {
+sleep(20);
+    C4::AR::Debug::debug("CRON => reindexar.pl!!!!!");
+	if ($ENV{'REMOTE_ADDR'} eq '127.0.0.1') {
+	    C4::AR::Sphinx::reindexar();
+	} else {
+	    C4::AR::Debug::debug("reindexar => se intento correr script de una dir. IP no local => ".$ENV{'REMOTE_ADDR'});
+	} 
 
-if ($ENV{'REMOTE_ADDR'} eq '127.0.0.1') {
-    C4::AR::Sphinx::reindexar();
-} else {
-    C4::AR::Debug::debug("reindexar => se intento correr script de una dir. IP no local => ".$ENV{'REMOTE_ADDR'});
-} 
+}else{
+	C4::AR::Debug::debug("reindexar => se intento correr script pero otra instancia se esta ejecutando. FALLO");
+}
+
+__DATA__
+This exists so flock() code above works.
+DO NOT REMOVE THIS DATA SECTION.
