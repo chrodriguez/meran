3ff596d7f087d3a0549971185911a34d12c9f656
Francisco Raposeiras <root@dev.redes>
2011-11-17 12:09:36 -0300

    eval en getDocument.pl en el OPAC y en la INTRA

--- intranet/cgi-bin/catalogacion/estructura/getDocument.pl
+++ intranet/cgi-bin/catalogacion/estructura/getDocument.pl
@@ -1,25 +1,5 @@
 #!/usr/bin/perl
 
-
-
-# Copyright 2000-2002 Katipo Communications
-#
-# This file is part of Koha.
-#
-# Koha is free software; you can redistribute it and/or modify it under the
-# terms of the GNU General Public License as published by the Free Software
-# Foundation; either version 2 of the License, or (at your option) any later
-# version.
-#
-# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along with
-# Koha; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
-# Suite 330, Boston, MA  02111-1307 USA
-#
-
 use strict;
 use C4::AR::Auth;
 use C4::Output;
@@ -30,33 +10,45 @@ my $input = new CGI;
 
 #SE PONE CUALQUIER TEMPLATE PARA EL CHEQUEO DE PERMISOS, PERO EL TEMPLATE NO SE USA
 my ($template, $session, $t_params)  = get_template_and_user({  
-                        template_name => "main.tmpl",
-                        query => $input,
-                        type => "intranet",
+                        template_name   => "main.tmpl",
+                        query           => $input,
+                        type            => "intranet",
                         authnotrequired => 0,
-                        flagsrequired => {  ui => 'ANY', 
-                                            tipo_documento => 'ANY', 
-                                            accion => 'CONSULTA', 
-                                            entorno => 'permisos', 
-                                            tipo_permiso => 'general'},
+                        flagsrequired   => {  ui            => 'ANY', 
+                                            tipo_documento  => 'ANY', 
+                                            accion          => 'CONSULTA', 
+                                            entorno         => 'permisos', 
+                                            tipo_permiso    => 'general'},
                         debug => 1,
                     });
 
-my $file_id=$input->param('id');
-my $eDocsDir = C4::Context->config('edocsdir');
-my $file = C4::AR::Catalogacion::getDocumentById($file_id);
-my $tmpFileName = $eDocsDir.'/'.$file->getFilename;
+# por si meten por URL un id no valido                   
+eval{                
 
-open INF, $tmpFileName or die "\nCan't open $tmpFileName for reading: $!\n";
+    my $file_id         = $input->param('id');
+    my $eDocsDir        = C4::Context->config('edocsdir');
+    my $file            = C4::AR::Catalogacion::getDocumentById($file_id);
+    my $tmpFileName     = $eDocsDir.'/'.$file->getFilename;
 
-print $input->header(
-                            -type           => $file->getFileType, 
-                            -attachment     => $file->getTitle,
-                            -expires        => '0',
-                    );
-my $buffer;
+    open INF, $tmpFileName or die "\nCan't open $tmpFileName for reading: $!\n";
 
-#SE ESCRIBE EL ARCHIVO EN EL CLIENTE
-while (read (INF, $buffer, 65536) and print $buffer ) {};
+    print $input->header(
+                                -type           => $file->getFileType, 
+                                -attachment     => $file->getTitle,
+                                -expires        => '0',
+                        );
+    my $buffer;
 
+    #SE ESCRIBE EL ARCHIVO EN EL CLIENTE
+    while (read (INF, $buffer, 65536) and print $buffer ) {};
 
+};
+
+# redirigimos
+if($@){
+
+    my $session = CGI::Session->load();
+
+    C4::AR::Auth::redirectTo(C4::AR::Utilidades::getUrlPrefix() . '/mainpage.pl?token=' . $session->param('token'));
+  
+}
