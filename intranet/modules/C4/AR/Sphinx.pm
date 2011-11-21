95f078d51850e220d89ecfd7d63ebfb088f30cb8
Matías Pagano <matiasp@mail.linti.unlp.edu.ar>
2011-11-21 12:44:31 -0300

    Fix del indexar bug #3435

--- intranet/modules/C4/AR/Sphinx.pm
+++ intranet/modules/C4/AR/Sphinx.pm
@@ -235,19 +235,19 @@ while (my $registro_marc_n1 = $sth1->fetchrow_hashref ){
 
     my $autor = C4::AR::Catalogacion::getRefFromStringConArrobas($marc_record->subfield("100","a"));
     if ($autor){
-        $autor = C4::AR::Catalogacion::getDatoFromReferencia("100", "a", $autor, $registro_marc_n1->{'template'});
+        $autor = C4::AR::Catalogacion::getDatoFromReferencia("100", "a", $autor, $registro_marc_n1->{'template'},1);
         ($autor ne "NO_TIENE")?push (@autores,$autor):"";
     }
 
        $autor = C4::AR::Catalogacion::getRefFromStringConArrobas($marc_record->subfield("110","a"));
     if ($autor){
-        $autor = C4::AR::Catalogacion::getDatoFromReferencia("110", "a", $autor, $registro_marc_n1->{'template'});
+        $autor = C4::AR::Catalogacion::getDatoFromReferencia("110", "a", $autor, $registro_marc_n1->{'template'},1);
         ($autor ne "NO_TIENE")?push (@autores,$autor):"";
     }
 
        $autor = C4::AR::Catalogacion::getRefFromStringConArrobas($marc_record->subfield("111","a"));
     if ($autor){
-        $autor = C4::AR::Catalogacion::getDatoFromReferencia("111", "a", $autor, $registro_marc_n1->{'template'});
+        $autor = C4::AR::Catalogacion::getDatoFromReferencia("111", "a", $autor, $registro_marc_n1->{'template'},1);
         ($autor ne "NO_TIENE")?push (@autores,$autor):"";
     }
 
@@ -261,7 +261,7 @@ while (my $registro_marc_n1 = $sth1->fetchrow_hashref ){
             C4::AR::Debug::debug("C4::AR::Sphinx::generar_indice => autor adicional ".$au_ad);
 
             if ($autor){
-                $autor = C4::AR::Catalogacion::getDatoFromReferencia("700", "a", $autor, $registro_marc_n1->{'template'});
+                $autor = C4::AR::Catalogacion::getDatoFromReferencia("700", "a", $autor, $registro_marc_n1->{'template'},1);
                 ($autor ne "NO_TIENE")?push (@autores,$autor):"";
             }
         }
@@ -276,7 +276,7 @@ while (my $registro_marc_n1 = $sth1->fetchrow_hashref ){
           C4::AR::Debug::debug("C4::AR::Sphinx::generar_indice => autor adicional ".$au_ad);
 
               if ($autor){
-                $autor = C4::AR::Catalogacion::getDatoFromReferencia("710", "a", $autor, $registro_marc_n1->{'template'});
+                $autor = C4::AR::Catalogacion::getDatoFromReferencia("710", "a", $autor, $registro_marc_n1->{'template'},1);
                 ($autor ne "NO_TIENE")?push (@autores,$autor):"";
               }
           }
@@ -301,8 +301,9 @@ while (my $registro_marc_n1 = $sth1->fetchrow_hashref ){
 #             C4::AR::Debug::debug("generar_indice => campo => ".$field->tag);
 #             C4::AR::Debug::debug("generar_indice => subcampo => ".$subfield->[0]);
  eval{
-            $dato_ref                       = C4::AR::Catalogacion::getRefFromStringConArrobasByCampoSubcampo($campo, $subcampo, $dato);
-            $dato                           = C4::AR::Catalogacion::getDatoFromReferencia($campo, $subcampo, $dato_ref, $registro_marc_n1->{'template'});
+	        my $nivel                       = C4::AR::EstructuraCatalogacionBase::getNivelFromEstructuraBaseByCampoSubcampo($campo, $subcampo);
+            $dato_ref                       = C4::AR::Catalogacion::getRefFromStringConArrobasByCampoSubcampo($campo, $subcampo, $dato,$registro_marc_n1->{'template'},$nivel);
+            $dato                           = C4::AR::Catalogacion::getDatoFromReferencia($campo, $subcampo, $dato_ref, $registro_marc_n1->{'template'},$nivel);
      }; #END eval
      
 		if ($@){
