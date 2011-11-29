62bb26f5050366f89e6acda59f423f2900b4683c
Matías Pagano <matiasp@mail.linti.unlp.edu.ar>
2011-11-29 10:55:52 -0300

    varios2

--- intranet/cgi-bin/cron/recordatorio_prestamos_vto.pl
+++ intranet/cgi-bin/cron/recordatorio_prestamos_vto.pl
@@ -12,12 +12,14 @@ if (C4::AR::Preferencias::getValorPreferencia('reminderMail') == 1){
 
     C4::AR::Debug::debug("CRON => recordatorio_prestamos_vto.pl => Se verifica via CRON si es necesario enviar los mails de recordacion!!! TODAY=".$today);
 
-    if ($ENV{'REMOTE_ADDR'} eq '127.0.0.1') {
+    if ( ($ENV{'REMOTE_ADDR'} eq '127.0.0.1')  || (!$ENV{'REMOTE_ADDR'}) ){
         C4::AR::Prestamos::enviarRecordacionDePrestamo($today);
     } else {
         C4::AR::Debug::debug("recordatorio_prestamos => se intento correr script de una dir. IP no local => ".$ENV{'REMOTE_ADDR'});
     }
-
+   
+ 
+    C4::AR::Debug::debug("REMOTE ADDRESS DE ENVIAR RECORDATORIO PRESTAMOS: ".$ENV{'REMOTE_ADDR'});
 }
 
 1;
