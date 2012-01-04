fc33f0259c213cea156172d6fbac042f74ad99ca
Matías Pagano <matiasp@mail.linti.unlp.edu.ar>
2012-01-03 23:25:26 -0300

    script importacion

--- intranet/scripts/leer_iso2709.pl
+++ intranet/scripts/leer_iso2709.pl
@@ -2,14 +2,24 @@
 
 
 use MARC::Moose::Record;
-
 use MARC::Moose::Reader::File::Isis;
 
 my $reader = MARC::Moose::Reader::File::Isis->new(
     file   => 'biblio.iso', );
 while ( my $record = $reader->read() ) {
 
-     my $formater = MARC::Moose::Formater::Text->new();
- print $formater->format( $record );
 
+     for my $field ( @{$record->fields} ) {
+         if($field->tag < '010'){
+             #CONTROL FIELD
+                 print "CAMPO CONTROL > ".$field->tag;
+                 print "\n";
+             }
+             else {
+         for my $subfield ( @{$field->subf} ) {
+              print "CAMPO > ".$field->tag." SUBCAMPO > ". $subfield->[0]." ==> ".$subfield->[1]."\n";
+            }
+        }
+
+     }
 }
