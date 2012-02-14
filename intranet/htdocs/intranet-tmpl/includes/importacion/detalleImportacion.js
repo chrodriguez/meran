6af00406f4cc927b0c957494a14962aa4782e582
Matías Pagano <matiasp@mail.linti.unlp.edu.ar>
2012-02-14 16:18:53 -0300

    detalles

--- intranet/htdocs/intranet-tmpl/includes/importacion/detalleImportacion.js
+++ intranet/htdocs/intranet-tmpl/includes/importacion/detalleImportacion.js
@@ -21,7 +21,7 @@ function detalleImportacion(id_importacion){
     objAH.funcion           = 'changePage';
     objAH.tipoAccion        = "DETALLE";
     objAH.id_importacion  = id_importacion;
-    objAH.record_filter  =  $("input[@name=record_filter]:checked").val();
+    objAH.record_filter  =  $('#record_filter').val();
     objAH.sendToServer();
 
 }
