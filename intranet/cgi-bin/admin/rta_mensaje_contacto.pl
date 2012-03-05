b41952642169ffa97183232199992818052288c9
Francisco Raposeiras <root@dev.redes>
2012-03-05 15:37:50 -0300

    mail contacto

--- intranet/cgi-bin/admin/rta_mensaje_contacto.pl
+++ intranet/cgi-bin/admin/rta_mensaje_contacto.pl
@@ -36,7 +36,6 @@ $mail{'mail_to'}        = $email;
 $mail{'mail_subject'}   = 'Re: '. $asunto;
 
 use C4::Modelo::PrefUnidadInformacion;
-use MIME::Lite::TT::HTML;
 
 my $ui                  = C4::AR::Referencias::obtenerDefaultUI();
 my $nombre_ui           = $ui->getNombre();
@@ -46,19 +45,18 @@ my $mailMessage         = C4::AR::Filtros::i18n("Estimado/a: ")."<br/><br/>".$te
                                         "<br /><br /> $mensaje_contacto<br /><br />";
                                         
        
-$mail{'mail_message'}           = $mailMessage;
+$mail{'mail_message'}   = $mailMessage;
 
-my ($ok, $msg_error)            = C4::AR::Mail::send_mail(\%mail);
+my ($ok, $msg_error)    = C4::AR::Mail::send_mail(\%mail);
   
 my  $msg = C4::AR::Mensajes::create();
 if ($msg_error){
-          C4::AR::Mensajes::add($msg, {'codMsg'=> 'U427', 'params' => []} ) ;
-
+    C4::AR::Mensajes::add($msg, {'codMsg'=> 'U427', 'params' => []} ) ;
 }else{
-          C4::AR::Mensajes::add($msg, {'codMsg'=> 'U426', 'params' => []} ) ;
+    C4::AR::Mensajes::add($msg, {'codMsg'=> 'U426', 'params' => []} ) ;
 }
 
-my $infoOperacionJSON = to_json $msg;
+my $infoOperacionJSON   = to_json $msg;
 
 C4::AR::Auth::print_header($session);
 print $infoOperacionJSON;
