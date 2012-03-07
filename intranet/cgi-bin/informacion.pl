5153c5f80ffeb987aee8ffec2217ae3be7b924c8
Gaspar Rajoy <gaspar@gaspo-meran-2012.info.redes.unlp.edu.ar>
2012-03-07 10:10:51 -0300

    mensaje automatico generado para el sql.rev225

--- intranet/cgi-bin/informacion.pl
+++ intranet/cgi-bin/informacion.pl
@@ -16,8 +16,8 @@ my ($template, $t_params)= C4::Output::gettemplate("informacion.tmpl", 'intranet
 my $session = CGI::Session->load();
 ##En este pl, se muestran todos los mensajes al usuario con respecto a la falta de permisos,
 #sin destruir la sesion del usuario, permitiendo asi que navegue por donde tiene permisos
-C4::AR::Debug::debug("informacion.pl =>  codMsg: ".$session->param("codMsg"));
 $t_params->{'mensaje'}= C4::AR::Mensajes::getMensaje($session->param("codMsg"),'INTRA',[]);
+$t_params->{'mensaje_class'}= "alert-error";
 
 
 C4::AR::Auth::output_html_with_http_headers($template, $t_params, $session);
