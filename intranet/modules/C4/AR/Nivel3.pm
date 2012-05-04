7a9f82328c43346073581fbc705bd524541be12e
Francisco Raposeiras <root@dev.redes>
2012-05-04 11:40:21 -0300

    volumen en nav tabs

--- intranet/modules/C4/AR/Nivel3.pm
+++ intranet/modules/C4/AR/Nivel3.pm
@@ -593,6 +593,9 @@ sub detalleCompletoINTRA {
             $hash_nivel2->{'año_publicacion'}           = $nivel2_array_ref->[$i]->getAnio_publicacion;
             $hash_nivel2->{'volumen'}                   = $nivel2_array_ref->[$i]->getVolumen;
             
+            #si tiene volumen se le concatena una "v. ". HARDCODED!!
+            if($hash_nivel2->{'volumen'}){ "v. " . $hash_nivel2->{'volumen'}; }
+            
             #Para el google book preview
             $hash_nivel2->{'isbn'}        		        = $nivel2_array_ref->[$i]->getISBN;
             if(($nivel2_array_ref->[$i]->getISSN)&&(!$t_params->{'issn'})){
