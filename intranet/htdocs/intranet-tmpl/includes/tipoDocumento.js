a2ec6ed54d88cd3ffdd63e518d0eed60d335e723
Francisco Raposeiras <root@dev.redes>
2012-06-25 16:27:13 -0300

    tipo de doc finished

---
+++ intranet/htdocs/intranet-tmpl/includes/tipoDocumento.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+        mostrarTabla();
+    });
+
+    function mostrarTabla(){
+        objAH               = new AjaxHelper(updateMostrarTabla);
+        objAH.debug         = true;
+        objAH.showOverlay   = true;
+        objAH.url           = URL_PREFIX+"/catalogacion/tipoDocumentoDB.pl";
+        objAH.tipoAccion    = 'LISTAR';
+
+        objAH.sendToServer();
+    }
+
+    function updateMostrarTabla(responseText){
+        $("#resultTipoDocumento").html(responseText);
+    }
+
+    function modificarTipoDocumento(idTipoDoc){
+
+        objAH               = new AjaxHelper(updateModificarTipoDocumento);
+        objAH.debug         = true;
+        objAH.showOverlay   = true;
+        objAH.idTipoDoc     = idTipoDoc;
+        objAH.url           = URL_PREFIX+"/catalogacion/tipoDocumentoDB.pl";
+        objAH.tipoAccion    = 'SHOW_MOD_TIPO_DOC';
+  
+        objAH.sendToServer();
+
+    }
+
+    function updateModificarTipoDocumento(responseText){
+        $('#accionesTipoDocumento').html(responseText);
+        $('#accionesTipoDocumento').modal();
+    }
+
+    function eliminarTipoDocumento(idTipoDoc){
+        objAH               = new AjaxHelper(updateEliminarTipoDocumento);
+        objAH.debug         = true;
+        objAH.showOverlay   = true;
+        objAH.idTipoDoc     = idTipoDoc;
+        objAH.url           = URL_PREFIX+"/catalogacion/tipoDocumentoDB.pl";
+        objAH.tipoAccion    = 'DEL_TIPO_DOC';
+  
+        objAH.sendToServer();
+    }
+
+    function updateEliminarTipoDocumento(responseText){
+        var Messages = JSONstring.toObject(responseText);
+        setMessages(Messages);
+        mostrarTabla();
+    }
+
+    function guardarModificacion(){
+
+        $('#modTipoDocumento').submit();
+    }
