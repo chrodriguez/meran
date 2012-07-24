f13afb4359e232de31671ef8e9293996c503f040
Gaspar Rajoy <root@gaspo-meran-2012.info.redes.unlp.edu.ar>
2012-07-24 15:40:19 -0300

    varios

--- install/installer.pl
+++ install/installer.pl
@@ -1,14 +1,17 @@
 #!/usr/bin/perl
+use strict;
+use warnings;
 use CGI;
 
-print "Content-type:text/html\r\n\r\n";
-print '<html>';
-print '<head>';
-print '<title>Hello Word - First CGI Program</title>';
-print '</head>';
-print '<body>';
-print '<h2>Hello Word! This is my first CGI program</h2>';
-print '</body>';
-print '</html>';
+#http://perlmeme.org/tutorials/cgi_script.html
+
+my $q = new CGI;
+
+print $q->header;
+
+print $q->header;
+print $q->start_html;
+print $q->h1("Hello World!");
+print $q->end_html;
 
 1;
