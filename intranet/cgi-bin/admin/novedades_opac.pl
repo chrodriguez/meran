2f9ef88b4e17b3f7f14d5f181d892fa634c65eb9
Francisco Raposeiras <root@dev.redes>
2012-08-02 10:20:56 -0300

    fix novedades varios

--- intranet/cgi-bin/admin/novedades_opac.pl
+++ intranet/cgi-bin/admin/novedades_opac.pl
@@ -7,15 +7,15 @@ use C4::AR::Novedades;
 my $input = new CGI;
 
 my ($template, $session, $t_params) = get_template_and_user({
-									template_name => "admin/novedades_opac.tmpl",
-									query => $input,
-									type => "intranet",
+									template_name 	=> "admin/novedades_opac.tmpl",
+									query 			=> $input,
+									type 			=> "intranet",
 									authnotrequired => 0,
-									flagsrequired => {  ui => 'ANY', 
-                                                        tipo_documento => 'ANY', 
-                                                        accion => 'CONSULTA', 
-                                                        entorno => 'usuarios'},
-									debug => 1,
+									flagsrequired 	=> {  ui 			=> 'ANY', 
+                                                        tipo_documento 	=> 'ANY', 
+                                                        accion 			=> 'CONSULTA', 
+                                                        entorno 		=> 'usuarios'},
+									debug 			=> 1,
 			    });
 
 C4::AR::Auth::output_html_with_http_headers($template, $t_params, $session);
