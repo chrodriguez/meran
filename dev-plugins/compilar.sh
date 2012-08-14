c41f7d928d50d3a875ddc37057f24011627a968a
Gaspar Rajoy <root@gaspo-meran-2012.info.redes.unlp.edu.ar>
2012-08-14 11:18:38 -0300

    [UPDATE] Bootstrap 2.0.4

--- dev-plugins/compilar.sh
+++ dev-plugins/compilar.sh
@@ -14,13 +14,11 @@ if [ $# -eq 0 ]
 	
 export PATH;
 cd bootstrapless/opac/$TEMA/
-lessc --verbose --compress meran.less
-# > $OLDPWD/../opac/htdocs/opac-tmpl/temas/$TEMA/includes/opac.css
+lessc --verbose --compress meran.less > $OLDPWD/../opac/htdocs/opac-tmpl/temas/$TEMA/includes/opac.css
 cd $OLDPWD
 #Cuando se hagan los temas para las distintas bibliotecas para la intranet esta #asignacion variable TEMA debe borrars
 TEMA="default";
 #Hasta aca
 cd bootstrapless/intranet/$TEMA/ 
-lessc --verbose --compress meran.less  
-#> $OLDPWD/../intranet/htdocs/intranet-tmpl/temas/$TEMA/includes/intranet.css
+lessc --verbose --compress meran.less > $OLDPWD/../intranet/htdocs/intranet-tmpl/temas/$TEMA/includes/intranet.css
 cd $OLDPWD
