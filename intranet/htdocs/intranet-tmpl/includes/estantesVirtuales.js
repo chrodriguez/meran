8ae0a8e422cee45b8bd2829435449c9e633d6554
Gaspar Rajoy <root@gaspo-meran-2012.info.redes.unlp.edu.ar>
2012-08-24 13:13:18 -0300

    fix en Filtros:133:link_to

--- intranet/htdocs/intranet-tmpl/includes/estantesVirtuales.js
+++ intranet/htdocs/intranet-tmpl/includes/estantesVirtuales.js
@@ -98,7 +98,6 @@ function verSubEstantes(estante,padre){
         objAH.estante= estante;
         objAH.padre= padre;
         objAH.tipo= 'VER_SUBESTANTE';
-        objAH.async = false;
         objAH.sendToServer();
 }
 
@@ -106,13 +105,10 @@ function updateVerSubEstantes(responseText){
     if(objAH.padre == 0){
         $('#subestante').html(responseText);
         scrollTo('subestante');
-   //     $('.datos_tabla_div_estantes').hide();
     }
     else{
         $('#subestante-'+ objAH.padre).html(responseText);
         scrollTo('subestante-'+ objAH.padre);
-        zebra('datos_tabla');
-     //   $('.datos_tabla_div_subestante_'+objAH.padre).hide();
     }
 }
 
