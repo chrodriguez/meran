24958a60d6d4b6100c238a43c6ff12403071809a
Francisco Raposeiras <root@dev.redes>
2012-08-30 13:12:36 -0300

    reportes

--- intranet/cgi-bin/reports/circulacion.pl
+++ intranet/cgi-bin/reports/circulacion.pl
@@ -5,7 +5,9 @@ require Exporter;
 use C4::AR::Auth;
 use CGI;
 
-my $query = new CGI;
+my $query 	= new CGI;
+
+my $obj 	= $query->param('obj') || 0;
 
 my ($template, $session, $t_params) = C4::AR::Auth::get_template_and_user({
                                     template_name   => "reports/circulacion.tmpl",
@@ -15,8 +17,24 @@ my ($template, $session, $t_params) = C4::AR::Auth::get_template_and_user({
                                     flagsrequired   => {  ui            => 'ANY', 
                                                         tipo_documento  => 'ANY', 
                                                         accion          => 'CONSULTA', 
-                                                        entorno         => 'undefined'},
+                                                        entorno         => 'undefined' },
 });
+
+if (!$obj) {
+
+    $obj 					= $query->Vars;
+    $obj->{'ui'} 			= $obj->{'tipo_prestamo_name'};
+    $obj->{'item_type'} 	= $obj->{'categoria_socio_id'};
+    $obj->{'nivel_biblio'} 	= $obj->{'name_nivel_bibliografico'};
+    
+} else {
+
+    $obj 					= C4::AR::Utilidades::from_json_ISO($obj);
+
+}
+
+$obj->{'fecha_ini'} 		=  C4::AR::Filtros::i18n($obj->{'fecha_ini'});
+$obj->{'fecha_fin'} 		=  C4::AR::Filtros::i18n($obj->{'fecha_fin'});
 
 $t_params->{'comboDeCategorias'}    = C4::AR::Utilidades::generarComboCategoriasDeSocio();
 $t_params->{'comboDeTipoDoc'}       = C4::AR::Utilidades::generarComboTipoDeDocConValuesIds();
