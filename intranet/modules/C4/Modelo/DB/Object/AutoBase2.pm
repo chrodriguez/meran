408a12b1c16ab5351b0c949def1bc284929b9dd4
Gaspar Rajoy <root@gaspo-meran-2012.info.redes.unlp.edu.ar>
2012-10-17 09:43:39 -0300

    DESACTIVADA LA CACHE

--- intranet/modules/C4/Modelo/DB/Object/AutoBase2.pm
+++ intranet/modules/C4/Modelo/DB/Object/AutoBase2.pm
@@ -1,7 +1,7 @@
 package C4::Modelo::DB::Object::AutoBase2;
 
-#use base 'Rose::DB::Object';
-use base 'Rose::DB::Object::Cached';
+use base 'Rose::DB::Object';
+#use base 'Rose::DB::Object::Cached';
 
 
 use C4::Modelo::DB::AutoBase1;
