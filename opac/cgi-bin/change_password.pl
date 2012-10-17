fba322a473796dadcf5f8e833ad79571e12cf7e3
Francisco Raposeiras <root@dev.redes>
2012-10-17 12:54:18 -0300

    mensaje automatico generado para el sql.rev304

--- opac/cgi-bin/change_password.pl
+++ opac/cgi-bin/change_password.pl
@@ -8,50 +8,32 @@ use CGI::Session;
 
 my $input = new CGI;
 
-if(C4::AR::Preferencias::getValorPreferencia("permite_cambio_password_desde_opac")){
-    
-    my ($template, $session, $t_params)= get_template_and_user({
-                                        template_name   => "opac-main.tmpl",
-                                        query           => $input,
-                                        type            => "opac",
-                                        authnotrequired => 0,
-                                        change_password => 1, #-> indicamos a checkauth que ya estamos cambiando la password
-                                        flagsrequired   => {    ui              => 'ANY', 
-                                                                tipo_documento  => 'ANY', 
-                                                                accion          => 'CONSULTA', 
-                                                                entorno         => 'undefined'},
-                });   
-    
-    
-    my $session                     = CGI::Session->load();
-    my ($template, $t_params)       = C4::Output::gettemplate("opac-main.tmpl", 'opac');
-
-    $t_params->{'mensaje'}          = C4::AR::Mensajes::getMensaje($session->param("codMsg"),'OPAC',[]);
-    $t_params->{'nro_socio'}        = C4::AR::Auth::getSessionNroSocio();
-    $t_params->{'partial_template'} = "opac-change-password.inc";
-    $t_params->{'noAjaxRequests'}   = 1;
-	$t_params->{'nroRandom'}        = C4::AR::Auth::getSessionNroRandom();
-	
-	#preferencias para generar nueva password
-    $t_params->{'minPassLength'}            = C4::AR::Preferencias::getValorPreferencia('minPassLength');
-    $t_params->{'minPassSymbol'}            = C4::AR::Preferencias::getValorPreferencia('minPassSymbol');
-    $t_params->{'minPassAlpha'}             = C4::AR::Preferencias::getValorPreferencia('minPassAlpha');
-    $t_params->{'minPassNumeric'}           = C4::AR::Preferencias::getValorPreferencia('minPassNumeric');
-
-    C4::AR::Auth::output_html_with_http_headers($template, $t_params, $session);
-
-}else{
-    #no se permite el cambio de passoword desde el OPAC
-    my $authnotrequired = 0;
-    my ($template, $session, $t_params) = checkauth(    $input, 
-                                                        $authnotrequired,
-                                                        {   ui              => 'ANY', 
+my ($template, $session, $t_params)= get_template_and_user({
+                                    template_name   => "opac-main.tmpl",
+                                    query           => $input,
+                                    type            => "opac",
+                                    authnotrequired => 0,
+                                    change_password => 1, #-> indicamos a checkauth que ya estamos cambiando la password
+                                    flagsrequired   => {    ui              => 'ANY', 
                                                             tipo_documento  => 'ANY', 
                                                             accion          => 'CONSULTA', 
-                                                            entorno         => 'usuarios'
-                                                        },
-                                                        "opac",
-                            );
+                                                            entorno         => 'undefined'},
+            });   
 
-    C4::AR::Auth::redirectTo(C4::AR::Utilidades::getUrlPrefix().'/opac-user.pl?token='.$session->param('token'));
-}
+
+my $session                     = CGI::Session->load();
+my ($template, $t_params)       = C4::Output::gettemplate("opac-main.tmpl", 'opac');
+
+$t_params->{'mensaje'}          = C4::AR::Mensajes::getMensaje($session->param("codMsg"),'OPAC',[]);
+$t_params->{'nro_socio'}        = C4::AR::Auth::getSessionNroSocio();
+$t_params->{'partial_template'} = "opac-change-password.inc";
+$t_params->{'noAjaxRequests'}   = 1;
+$t_params->{'nroRandom'}        = C4::AR::Auth::getSessionNroRandom();
+
+#preferencias para generar nueva password
+$t_params->{'minPassLength'}            = C4::AR::Preferencias::getValorPreferencia('minPassLength');
+$t_params->{'minPassSymbol'}            = C4::AR::Preferencias::getValorPreferencia('minPassSymbol');
+$t_params->{'minPassAlpha'}             = C4::AR::Preferencias::getValorPreferencia('minPassAlpha');
+$t_params->{'minPassNumeric'}           = C4::AR::Preferencias::getValorPreferencia('minPassNumeric');
+
+C4::AR::Auth::output_html_with_http_headers($template, $t_params, $session);
