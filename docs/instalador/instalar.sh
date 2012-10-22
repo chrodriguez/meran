43bb4947ad2c45a9a7423a4920b4dd0d6f40b215
Gaspar Rajoy <root@gaspo01.(none)>
2012-10-22 12:38:16 -0300

    instalador meran

--- docs/instalador/instalar.sh
+++ docs/instalador/instalar.sh
@@ -68,14 +68,15 @@ generarConfiguracion()
 
 generarCrons()
 {
+ ADONDE=$DESTINO_MERAN/$ID/intranet/modules/C4
   crontab -l >/tmp/$ID.crontab
   echo "
-  0 7 * * *      export MERAN_CONF=$CONFIGURACION_MERAN/meran$ID.conf; cd $DESTINO_MERAN/$ID/intranet/modules/ ; perl ../cgi-bin/cron/recordatorio_prestamos_vto.pl 2>&1
-  0 * * * *      export MERAN_CONF=$CONFIGURACION_MERAN/meran$ID.conf; cd $DESTINO_MERAN/$ID/intranet/modules/ ; perl ../cgi-bin/cron/mail_prestamos_vencidos.pl  2>&1
-  * * * * *      export MERAN_CONF=$CONFIGURACION_MERAN/meran$ID.conf; cd $DESTINO_MERAN/$ID/intranet/modules/ ; perl ../cgi-bin/cron/procesarColaZ3950.pl  2>&1
-  * * * * *      export MERAN_CONF=$CONFIGURACION_MERAN/meran$ID.conf; cd $DESTINO_MERAN/$ID/intranet/modules/ ; perl ../cgi-bin/cron/reindexar.pl 2>&1
-  0 0 * * *      export MERAN_CONF=$CONFIGURACION_MERAN/meran$ID.conf; cd $DESTINO_MERAN/$ID/intranet/modules/ ; perl ../cgi-bin/cron/obtener_portadas_de_registros.pl  2>&1
-  0 0 * * 6      export MERAN_CONF=$CONFIGURACION_MERAN/meran$ID.conf; cd $DESTINO_MERAN/$ID/intranet/modules/ ; perl ../cgi-bin/cron/generar_indice.pl  2>&1" >>/tmp/$ID.crontab
+  0 7 * * *      export PERL5LIB=$ADONDE/Share/share/perl/5.10.1/:$ADONDE/Share/lib/perl/5.10.1/:$ADONDE/Share/share/perl/5.10/:$ADONDE/C4/Share/share/perl/5.10.1/:$ADONDE/Share/lib/perl/5.10/:$ADONDE/Share/lib/perl5/; export MERAN_CONF=$CONFIGURACION_MERAN/meran$ID.conf; cd $DESTINO_MERAN/$ID/intranet/modules/ ; perl ../cgi-bin/cron/recordatorio_prestamos_vto.pl 2>&1
+  0 * * * *      export PERL5LIB=$ADONDE/Share/share/perl/5.10.1/:$ADONDE/Share/lib/perl/5.10.1/:$ADONDE/Share/share/perl/5.10/:$ADONDE/C4/Share/share/perl/5.10.1/:$ADONDE/Share/lib/perl/5.10/:$ADONDE/Share/lib/perl5/; export MERAN_CONF=$CONFIGURACION_MERAN/meran$ID.conf; cd $DESTINO_MERAN/$ID/intranet/modules/ ; perl ../cgi-bin/cron/mail_prestamos_vencidos.pl  2>&1
+  * * * * *      export PERL5LIB=$ADONDE/Share/share/perl/5.10.1/:$ADONDE/Share/lib/perl/5.10.1/:$ADONDE/Share/share/perl/5.10/:$ADONDE/C4/Share/share/perl/5.10.1/:$ADONDE/Share/lib/perl/5.10/:$ADONDE/Share/lib/perl5/; export MERAN_CONF=$CONFIGURACION_MERAN/meran$ID.conf; cd $DESTINO_MERAN/$ID/intranet/modules/ ; perl ../cgi-bin/cron/procesarColaZ3950.pl  2>&1
+  * * * * *      export PERL5LIB=$ADONDE/Share/share/perl/5.10.1/:$ADONDE/Share/lib/perl/5.10.1/:$ADONDE/Share/share/perl/5.10/:$ADONDE/C4/Share/share/perl/5.10.1/:$ADONDE/Share/lib/perl/5.10/:$ADONDE/Share/lib/perl5/; export MERAN_CONF=$CONFIGURACION_MERAN/meran$ID.conf; cd $DESTINO_MERAN/$ID/intranet/modules/ ; perl ../cgi-bin/cron/reindexar.pl 2>&1
+  0 0 * * *      export PERL5LIB=$ADONDE/Share/share/perl/5.10.1/:$ADONDE/Share/lib/perl/5.10.1/:$ADONDE/Share/share/perl/5.10/:$ADONDE/C4/Share/share/perl/5.10.1/:$ADONDE/Share/lib/perl/5.10/:$ADONDE/Share/lib/perl5/; export MERAN_CONF=$CONFIGURACION_MERAN/meran$ID.conf; cd $DESTINO_MERAN/$ID/intranet/modules/ ; perl ../cgi-bin/cron/obtener_portadas_de_registros.pl  2>&1
+  0 0 * * 6      export PERL5LIB=$ADONDE/Share/share/perl/5.10.1/:$ADONDE/Share/lib/perl/5.10.1/:$ADONDE/Share/share/perl/5.10/:$ADONDE/C4/Share/share/perl/5.10.1/:$ADONDE/Share/lib/perl/5.10/:$ADONDE/Share/lib/perl5/; export MERAN_CONF=$CONFIGURACION_MERAN/meran$ID.conf; cd $DESTINO_MERAN/$ID/intranet/modules/ ; perl ../cgi-bin/cron/generar_indice.pl  2>&1" >>/tmp/$ID.crontab
   crontab /tmp/$ID.crontab
 
 }
