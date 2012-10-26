1aba5115d61af773a3164ee6264c3ed7ddc71988
Gaspar Rajoy <root@gaspo-meran-2012.info.redes.unlp.edu.ar>
2012-10-26 19:31:16 -0300

    NUEVO INSTALADOR

--- docs/instalador/instalar.sh
+++ docs/instalador/instalar.sh
@@ -63,7 +63,7 @@ generarConfiguracion()
   sed s/reemplazarUSER/$(escaparVariable $USER_BDD_MERAN)/g /tmp/$ID.meran.conf > /tmp/$ID.meran2.conf
   sed s/reemplazarPASS/$(escaparVariable $PASS_BDD_MERAN)/g /tmp/$ID.meran2.conf > /tmp/$ID.meran.conf
   sed s/reemplazarPATHBASE/$(escaparVariable $DESTINO_MERAN)/g  /tmp/$ID.meran.conf > /tmp/$ID.meran2.conf
-  sed s/reemplazarCONFMERAN/$(escaparVariable $CONFIGURACION_MERAN)/g /tmp/$ID.meran2.conf > /$ID.meran3.conf
+  sed s/reemplazarCONFMERAN/$(escaparVariable $CONFIGURACION_MERAN)/g /tmp/$ID.meran2.conf > /tmp/$ID.meran3.conf
   sed s/reemplazarDATABASE/$(escaparVariable $BDD_MERAN)/g /tmp/$ID.meran3.conf > $CONFIGURACION_MERAN/meran$ID.conf
   rm /tmp/$ID.meran*
   generarScriptDeInicio
