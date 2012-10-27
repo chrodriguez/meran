bd4d3c831c47f22e645c7e88e3b6a749a83a9eb0
Gaspar Rajoy <root@gaspo-meran-2012.info.redes.unlp.edu.ar>
2012-10-27 00:13:04 -0300

    FIXEADO el generador realmente

--- docs/generacionInstalador/new_instalador.sh
+++ docs/generacionInstalador/new_instalador.sh
@@ -19,7 +19,7 @@ done;
 mkdir meranunlp
 cp ../../instalador/* meranunlp
 rm meranunlp/intranetyopac.tar.gz
-cp README COPYING licencia.txt meranunlp/
+cp ../README ../COPYING licencia.txt meranunlp/
 echo $VERSION > meranunlp/VERSION
 tar -czvf meranunlp/intranetyopac.tar.gz opac/ intranet/ includes/
 tar -czvf meranunlp-v$VERSION.tar.gz meranunlp
