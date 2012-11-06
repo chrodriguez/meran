ed9ebcf35d876edb7099f03daa1d375ffc6e81c9
Gaspar Rajoy <root@gaspo-meran-2012.info.redes.unlp.edu.ar>
2012-11-06 16:08:52 -0300

    Test

--- gitLog.pl
+++ gitLog.pl
@@ -1,3 +1,4 @@
 #!/bin/bash
+#JO
 git log  --pretty=oneline --grep="HOTFIX" --all  --since="$1 days ago" | cut -c42-1000 | awk '!x[$0]++'
 git log  --pretty=oneline --grep="CHANGELOG" --all  --since="$1 days ago" | cut -c42-1000 | awk '!x[$0]++'
