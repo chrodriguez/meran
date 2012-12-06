a735f78b13615d14d1d4cadb36d7740bdc2cac4a
root <root@maquina107.linti.unlp.edu.ar>
2012-12-06 11:49:57 -0300

    Test para ver usuario commiter

--- gitLog.pl
+++ gitLog.pl
@@ -1,3 +1,2 @@
 #!/bin/bash 
 git log  --pretty=oneline --all  --since="$1 days ago" | cut -c42-1000 | awk '!x[$0]++'
-#git log  --pretty=oneline  --all  --since="$1 days ago" | cut -c42-1000 | awk '!x[$0]++'
